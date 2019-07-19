2f9c0071c497633365778c6b06e4621b5e0ca26f
KunPengZheng <zhengwp@adnonstop.com>
2019-07-19 19:15:36 +0800

    demo2

--- app/src/main/java/com/zkp/breath/rxjava2/Demo.java
+++ app/src/main/java/com/zkp/breath/rxjava2/Demo.java
@@ -6,7 +6,9 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created b Zwp on 2019/7/18.
@@ -45,6 +47,39 @@ public class Demo {
                 Log.d("demo1", "onComplete:");
             }
         });
+    }
+
+    public void demo2() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
 
