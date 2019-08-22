712a325cd552b0e5707bcbaa2d9af646cac8aa64
KunPengZheng <zhengwp@adnonstop.com>
2019-08-22 14:31:48 +0800

    arraylist的demo

--- app/src/main/java/com/zkp/breath/review/Demo.java
+++ app/src/main/java/com/zkp/breath/review/Demo.java
@@ -1,5 +1,9 @@
 package com.zkp.breath.review;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created b Zwp on 2019/8/22.
  */
@@ -53,10 +57,44 @@ public class Demo {
         System.out.println(i3 == i4);   // false。内存比较，i4超过缓存，重新new
         System.out.println((i3 + i5) == i4); // true。两边有一边是表达式（包含算数运算）则 == 比较的是数值（自动触发拆箱过程）
 
-
         System.out.println();
-        System.out.println("===========String字符串===========");
+        System.out.println("===========ArrayList===========");
+
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+        integerArrayList.add(1);
+        integerArrayList.add(2);
+        integerArrayList.add(3);
+        // 数组是new的，但是元素浅拷贝
+        Integer[] integers1 = integerArrayList.toArray(new Integer[3]);
+        Integer[] integers2 = integerArrayList.toArray(new Integer[2]);
+        System.out.println(Arrays.equals(integers1, integers2));    // 源码最终调用元素的equals()方法
+        System.out.println();
+
+        Integer[] integers = {1, 2, 3};
+        List<Integer> integers3 = Arrays.asList(integers);  // 返回Arrays内部类ArrayList,不具备增删操作
+        try {
+            integers3.add(4);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("不具备该操作" + e.toString());
+        }
+        List<Integer> integers4 = new ArrayList<>(integers3);   // 转换为真正的ArrayList,具备增删操作
+        integers4.add(4);
+        System.out.println();
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("java");
+        stringList.add("android");
+        // 内部使用toArray()，集合的内存地址不同，但指向相同的元素（元素属于浅拷贝）
+        List<String> stringList1 = new ArrayList<>(stringList);
+        System.out.println(stringList.equals(stringList1));
+
+        // 生成子集的后续操作会对父集生效，看源码
+        List<String> subList = stringList.subList(0, stringList.size());
+        subList.add("c++");
+        System.out.println(stringList.toString());
+
+
 
     }
-
 }
