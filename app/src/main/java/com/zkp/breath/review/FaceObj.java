e33309ff329da211ca27d8e4ece75e5b303e897b
KunPengZheng <zhengwp@adnonstop.com>
2019-08-23 16:38:42 +0800

    添加demo

--- app/src/main/java/com/zkp/breath/review/FaceObj.java
+++ app/src/main/java/com/zkp/breath/review/FaceObj.java
@@ -1,8 +1,7 @@
 package com.zkp.breath.review;
 
 /**
- * Created b Zwp on 2019/8/23.
- * 多态（1.继承 2.重写 3.父类引用指向子类对象）
+ * Created b Zwp on 2019/8/23. 多态（1.继承 2.重写 3.父类引用指向子类对象）
  */
 public class FaceObj {
 
@@ -30,6 +29,23 @@ public class FaceObj {
         System.out.println(b.run(b));   // "B & B"
         System.out.println(b.run(c));   // "B & B"
         System.out.println(b.run(d));   // "A & D"
+        System.out.println();
+
+        Object obj1 = new Demo();   // 多态
+        Object obj2 = new Demo();   // 多态
+
+        Demo obj3 = new Demo();
+        Demo obj4 = new Demo();
+
+        // 多态，编译看左边，检查到子类没有重写，所以最终调用了Object的equals方法（比较的是内存地址），所以判断条件为false
+        if (obj1.equals(obj2)) {
+            System.out.println("obj1和obj2");
+        }
+
+        // 非多态，选择最吻合的方法，子类重载的方法就能接收obj4，就不要去调用Object的equals方法，所以最终走了重载方法
+        if (obj3.equals(obj4)) {
+            System.out.println("obj1和obj2");
+        }
     }
 
     private static class A {
@@ -63,5 +79,13 @@ public class FaceObj {
 
     private static class D extends B {
 
+    }
+
+    private static class Demo {
+        // 分析：下面的类是重载而不是重写，因为违反了参数规则，重写是方法体不一致，其余都要相同，而下面的方法参数修改过了，所以是重载
+        public boolean equals(Demo other) {
+            System.out.println("use Demo equals.");
+            return true;
+        }
     }
 }
