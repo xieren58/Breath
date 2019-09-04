cfbf168c717c1a7ce72377ab13268ee912757cb0
zkp <1258520250@qq.com>
2019-09-04 21:56:11 +0800

    条件控制Demo

---
+++ app/src/main/java/com/zkp/breath/kotlin/classz/ConditionControlDemo.kt
@@ -0,0 +1,40 @@
+package com.zkp.breath.kotlin.classz
+
+// 条件控制Demo
+
+fun f1(x: Int) {
+    // when 类似其他语言的 switch 操作符。在 when 中，else 同 switch 的 default
+    when (x) {
+        1 -> print("x == 1")
+        2 -> print("x == 2")
+        else -> { // 注意这个块
+            print("x 不是 1 ，也不是 2")
+        }
+    }
+}
+
+fun f2(x: Int) {
+// 如果很多分支需要用相同的方式处理，则可以把多个分支条件放在一起，用逗号分隔
+    when (x) {
+        0, 1 -> print("x == 0 or x == 1")
+        else -> print("otherwise")
+    }
+}
+
+fun f3(x: Int) {
+// 配合in使用
+    when (x) {
+        in 1..10 -> print("x is in the range")
+        in 30..40 -> print("x is valid")
+        !in 10..20 -> print("x is outside the range")
+        else -> print("none of the above")
+    }
+}
+
+
+// 配合is使用，一旦判断成功则自动转换该类型，就能使用该类型的方法或属性
+// 下面的方法存在隐式返回值
+fun hasPrefix(x: Any) = when (x) {
+    is String -> x.startsWith("prefix")
+    else -> false
+}
