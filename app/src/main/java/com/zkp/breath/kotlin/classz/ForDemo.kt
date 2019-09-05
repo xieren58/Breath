2439aa57b8565e7b7ff51691874d9661366076f9
zkp <1258520250@qq.com>
2019-09-05 15:57:50 +0800

    区间demo

--- app/src/main/java/com/zkp/breath/kotlin/classz/ForDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/classz/ForDemo.kt
@@ -22,4 +22,26 @@ fun main() {
         println("the element at $index is $value")
     }
 
+    // ================= 区间 =================
+    // ================= 区间 =================
+
+    for (i in 1..4) print(i) // 输出“1234”
+
+    for (i in 4 downTo 1) print(i)
+
+    val x = 20
+    if (x in 1..10) { // 等同于 1 <= i && i <= 10
+        println(x)
+    }
+
+// 使用 step 指定步长
+    for (i in 1..4 step 2) print(i) // 输出“13”
+
+    for (i in 4 downTo 1 step 2) print(i) // 输出“42”，
+
+// 使用 until 函数排除结束元素
+    for (i in 1 until 10) {   // i in [1, 10) 排除了 10
+        println(i)
+    }
+
 }
