a2384c47b8879606404c7cb4e2ab3d1ce4c0c00e
zkp <1258520250@qq.com>
2019-09-06 23:58:39 +0800

    类型别名Dem

---
+++ app/src/main/java/com/zkp/breath/kotlin/classz/DelegationDemo.kt
@@ -0,0 +1,18 @@
+package com.zkp.breath.kotlin.classz
+
+interface S {
+    fun getTask()
+}
+
+class MI(name: String) : S {
+    override fun getTask() {
+        println("子类重写")
+    }
+}
+
+class Proxy(one: S) : S by one
+
+fun main(args: Array<String>) {
+    val one = MI("小明")
+    Proxy(one).getTask()
+}
