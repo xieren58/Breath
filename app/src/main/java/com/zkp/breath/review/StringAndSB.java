b27f48748b30dcf101973612e3420e2d570f8670
zkp <1258520250@qq.com>
2019-09-19 18:03:12 +0800

    测试string的trim方法

--- app/src/main/java/com/zkp/breath/review/StringAndSB.java
+++ app/src/main/java/com/zkp/breath/review/StringAndSB.java
@@ -21,10 +21,28 @@ public class StringAndSB {
         // false,StringBuffer的toString()会new出一个String，new出来的内存地址一定不相等
         System.out.println("丢4" + (s2.toString() == s1));
 
-        String str1 ="123";
+        String str1 = "123";
         // 看源码，0直接返回本身
         System.out.println("123" == str1.substring(0)); //3,true
         // 其余会返回一个新new出来的对象
         System.out.println("23" == str1.substring(1));  //4,false
+
+        trimMethod();
     }
+
+    private static void trimMethod() {
+        // trim内部调用了substring
+        String str = "ab";
+        String str1 = (" a" + "b ").trim();  // 左右有空格发生截取，重新new
+        String str2 = ("a" + "b").trim();   // 没有发生截取，返回本身
+        System.out.println(str == str1);    // false
+        System.out.println(str == str2);    // true
+
+
+        String s3 = " a bcde ";
+        System.out.println("输出:" + s3);
+
+    }
+
+
 }
