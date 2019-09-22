0877f2e4b0670474be1447e55589baf422142db7
zkp <1258520250@qq.com>
2019-09-22 16:48:21 +0800

    如果向线程池提交了多个任务，获取顺序是任意的，取决于任务的完成顺序

--- app/src/main/java/com/zkp/breath/review/threads/ExecutorsDemo.java
+++ app/src/main/java/com/zkp/breath/review/threads/ExecutorsDemo.java
@@ -3,20 +3,46 @@ package com.zkp.breath.review.threads;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class ExecutorsDemo {
 
-    public static void main(String[] args) {
+    private static void executorCompletionService() {
+        // 如果向线程池提交了多个任务，获取顺序是任意的，取决于任务的完成顺序
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        CompletionService<Integer> executorCompletionService = new ExecutorCompletionService<>(executorService);
+        int length = 0;
+
+        for (int i = 0; i < 10000; i++) {
+            executorCompletionService.submit(new CallableImp(i));
+            length++;
+        }
+
+        int finalLength = length;
+        new Thread(() -> {
+            for (int i = 0; i < finalLength; i++) {
+                try {
+                    Future<Integer> take = executorCompletionService.take();
+                    Integer integer = take.get();
+                    System.out.println("获取到的返回值 : " + integer);
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    private static void submit() {
         ExecutorService executorService = Executors.newCachedThreadPool();
         List<Future<Integer>> list = new ArrayList<>();
         for (int i = 0; i < 10000; i++) {
-            // 无返回值提交任务
-//            executorService.execute(new RunnableImp(i));
-
             // 有返回值提交
             Future<Integer> submit = executorService.submit(new CallableImp(i));
             list.add(submit);
@@ -36,6 +62,20 @@ public class ExecutorsDemo {
             }
 
         }).start();
+    }
+
+    private static void execute() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10000; i++) {
+            // 无返回值提交任务
+            executorService.execute(new RunnableImp(i));
+        }
+    }
+
+    public static void main(String[] args) {
+//        execute();
+//        submit();
+        executorCompletionService();
     }
 
     private static class RunnableImp implements Runnable {
