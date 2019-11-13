b070fd8571ef06f88e626b2e99328fd1395b2870
zkp <1258520250@qq.com>
2019-11-14 00:28:42 +0800

    删除腾讯云im依赖

--- app/src/main/java/com/zkp/breath/BaseApplication.java
+++ app/src/main/java/com/zkp/breath/BaseApplication.java
@@ -2,12 +2,6 @@ package com.zkp.breath;
 
 import androidx.multidex.MultiDexApplication;
 
-import com.tencent.imsdk.TIMSdkConfig;
-import com.tencent.qcloud.tim.uikit.TUIKit;
-import com.tencent.qcloud.tim.uikit.config.CustomFaceConfig;
-import com.tencent.qcloud.tim.uikit.config.GeneralConfig;
-import com.tencent.qcloud.tim.uikit.config.TUIKitConfigs;
-
 /**
  * Created b Zwp on 2019/7/25.
  */
@@ -18,20 +12,5 @@ public class BaseApplication extends MultiDexApplication {
         super.onCreate();
         // 常用工具库初始化
         com.blankj.utilcode.util.Utils.init(this);
-        TUIKitInit();
-    }
-
-    /**
-     * 腾讯云IM初始化（tuikit版本，包含UI）
-     */
-    private void TUIKitInit() {
-        // 配置 Config，请按需配置
-        TUIKitConfigs configs = TUIKit.getConfigs();
-        int sdkAppID = 1400266626;
-        configs.setSdkConfig(new TIMSdkConfig(sdkAppID));
-        configs.setCustomFaceConfig(new CustomFaceConfig());
-        configs.setGeneralConfig(new GeneralConfig());
-
-        TUIKit.init(this, 1400266626, configs);
     }
 }
