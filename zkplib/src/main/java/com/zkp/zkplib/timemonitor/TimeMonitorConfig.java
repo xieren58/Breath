e53a9c91ff14c79d3614c2dbc5f57cb8daad9ea9
zkp <1258520250@qq.com>
2019-12-30 09:53:00 +0800

    添加耗时统计模板类

---
+++ zkplib/src/main/java/com/zkp/zkplib/timemonitor/TimeMonitorConfig.java
@@ -0,0 +1,7 @@
+package com.zkp.zkplib.timemonitor;
+
+public class TimeMonitorConfig {
+
+    // 应用启动耗时
+    public static final int TIME_MONITOR_ID_APPLICATION_START = 1;
+}
---
+++ zkplib/src/main/java/com/zkp/zkplib/timemonitor/TimeMonitorManager.java
@@ -0,0 +1,42 @@
+package com.zkp.zkplib.timemonitor;
+
+import java.util.HashMap;
+
+public class TimeMonitorManager {
+
+    private static TimeMonitorManager mTimeMonitorManager = null;
+    private HashMap<Integer, TimeMonitor> mTimeMonitorMap = null;
+
+    public synchronized static TimeMonitorManager getInstance() {
+        if (mTimeMonitorManager == null) {
+            mTimeMonitorManager = new TimeMonitorManager();
+        }
+        return mTimeMonitorManager;
+    }
+
+    public TimeMonitorManager() {
+        this.mTimeMonitorMap = new HashMap<Integer, TimeMonitor>();
+    }
+
+    /**
+     * 初始化打点模块
+     */
+    public void resetTimeMonitor(int id) {
+        if (mTimeMonitorMap.get(id) != null) {
+            mTimeMonitorMap.remove(id);
+        }
+        getTimeMonitor(id);
+    }
+
+    /**
+     * 获取打点器
+     */
+    public TimeMonitor getTimeMonitor(int id) {
+        TimeMonitor monitor = mTimeMonitorMap.get(id);
+        if (monitor == null) {
+            monitor = new TimeMonitor(id);
+            mTimeMonitorMap.put(id, monitor);
+        }
+        return monitor;
+    }
+}
