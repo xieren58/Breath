997db874d0a7b298e4b62d0698f8c557d1e1c2f9
zkp <1258520250@qq.com>
2020-01-02 15:49:38 +0800

    提升gradle版本

--- app/src/main/java/com/zkp/breath/component/activity/ActivityCoordinator.kt
+++ app/src/main/java/com/zkp/breath/component/activity/ActivityCoordinator.kt
@@ -26,6 +26,10 @@ class ActivityCoordinator : AppCompatActivity() {
 
     private fun initRecycleView() {
         val arrayListOf = arrayListOf("1", "2", "3", "5", "6", "7", "8", "9", "10")
+        //当知道Adapter内Item的改变不会影响RecyclerView宽高的时候，可以设置为true让RecyclerView避免重新计算大小
+        recyclerView?.setHasFixedSize(true)
+        recyclerView?.isNestedScrollingEnabled = false
+        recyclerView?.overScrollMode = View.OVER_SCROLL_NEVER
         recyclerView?.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         recyclerView?.adapter = CoordinatorAdapter(arrayListOf)
     }
