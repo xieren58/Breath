4ba815699ebc3579c3ee9e5153ba38dd38659ef5
zkp <1258520250@qq.com>
2020-01-02 17:03:43 +0800

    添加CollapsingToolbarLayout的demo

--- app/src/main/java/com/zkp/breath/component/activity/ActivityCoordinator.kt
+++ app/src/main/java/com/zkp/breath/component/activity/ActivityCoordinator.kt
@@ -1,6 +1,8 @@
 package com.zkp.breath.component.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,6 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.zkp.breath.CoordinatorAdapter
 import com.zkp.breath.R
 
+
+/**
+ * https://www.jianshu.com/p/bbc703a0015e
+ * https://www.jianshu.com/p/eec5a397ce79
+ */
 class ActivityCoordinator : AppCompatActivity() {
 
     var recyclerView: RecyclerView? = null
@@ -15,22 +22,38 @@ class ActivityCoordinator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator)
-
-        recyclerView = findViewById(R.id.recyclerView)
-        init()
-    }
-
-    private fun init() {
         initRecycleView()
     }
 
     private fun initRecycleView() {
+        recyclerView = findViewById(R.id.recyclerView)
         val arrayListOf = arrayListOf("1", "2", "3", "5", "6", "7", "8", "9", "10")
         //当知道Adapter内Item的改变不会影响RecyclerView宽高的时候，可以设置为true让RecyclerView避免重新计算大小
         recyclerView?.setHasFixedSize(true)
         recyclerView?.overScrollMode = View.OVER_SCROLL_NEVER
         recyclerView?.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         recyclerView?.adapter = CoordinatorAdapter(arrayListOf)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add("AppBarLayout").setTitle("切换AppBarLayout布局").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        menu.add("CollapsingToolbarLayout").setTitle("切换CollapsingToolbarLayout布局").setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val name = item.title.toString()
+        when (name) {
+            "切换AppBarLayout布局" -> {
+                setContentView(R.layout.activity_coordinator)
+                initRecycleView()
+            }
+            "切换CollapsingToolbarLayout布局" -> {
+                setContentView(R.layout.activity_collapsing)
+                initRecycleView()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
