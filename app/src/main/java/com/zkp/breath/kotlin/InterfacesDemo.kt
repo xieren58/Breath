601840ad24290c5407fc46e987bb9905754f0fc6
zkp <1258520250@qq.com>
2020-01-07 16:15:13 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/InterfacesDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/InterfacesDemo.kt
@@ -26,21 +26,9 @@ class AsImp2 : AS {
     }
 }
 
-// 知识点5
-fun main(args: Array<String>) {
-
-
-    val b = B1()
-    b.a = "你们形式上"   // 在1和2的声明方式下打印出来的还是A的值
-    println(b.a)
-
-}
-
 interface A1 {
     var a: String
         get() = "我们"
-        //  Property in an interface cannot have a backing field，一个接口不能有幕后字段的功能
-        // var属性提供了get，则要提供set，如果不提供set的话会编译器认为是有幕后字段，而接口的属性的不能有幕后字段的
         set(value) {}
 
     val bb: String
@@ -48,10 +36,6 @@ interface A1 {
 }
 
 class B1 : A1 {
-    //   as自动提供get/set，不能提供初始值，否则会编译器报错
-//    override var a: String
-//        get() = super.a
-//        set(value) {}
 
     override var a: String = ""
 
@@ -59,3 +43,12 @@ class B1 : A1 {
         get() = super.bb
 
 }
+
+// 知识点5
+fun main(args: Array<String>) {
+    val b = B1()
+    b.a = "你们形式上"   // 在1和2的声明方式下打印出来的还是A的值
+    println(b.a)
+
+}
+
