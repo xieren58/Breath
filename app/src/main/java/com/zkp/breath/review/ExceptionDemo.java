7eff3a12aa3874f2e4254ce3f3c91d9029a38faa
zkp <1258520250@qq.com>
2020-02-21 11:35:27 +0800

    添加注释

--- app/src/main/java/com/zkp/breath/review/ExceptionDemo.java
+++ app/src/main/java/com/zkp/breath/review/ExceptionDemo.java
@@ -37,7 +37,7 @@ public class ExceptionDemo {
         System.out.println();
         System.out.println();
 
-        System.out.println(t1());
+        System.out.println("t1()方法返回值:" + t1());
         System.out.println(t2());   // 返回值为finally的
         System.out.println(t3());   // 原异常被覆盖
     }
@@ -62,15 +62,30 @@ public class ExceptionDemo {
         throw new NullPointerException();
     }
 
+    /**
+     * 方法运行返回 0，因为执行到 try 的 return i; 语句前会先将返回值 i 保存在一个临时变量中，
+     * 然后再执行 finally 语句，最后再返回那个临时变量，finally 中对 i 的修改不会被返回。
+     */
     private static int t1() {
         int i = 0;
         try {
             return i;
         } finally {
             i = 2;
+            System.out.println("执行到了t1()方法的finally块");
         }
     }
 
+    /**
+     * finally语块的return覆盖了try语块的return，一旦执行了return则方法结束
+     * 解析：
+     * private static int t2() {
+     * int i = 0;
+     * int tempI = 0;
+     * return 2;
+     * return tempI
+     * }
+     */
     private static int t2() {
         int i = 0;
         try {
@@ -81,6 +96,9 @@ public class ExceptionDemo {
         }
     }
 
+    /**
+     * 方法运行抛出 hello 异常，因为如果 finally 中抛出了异常，则原异常就会被掩盖。
+     */
     private static int t3() {
         try {
             int i = 5 / 0;
