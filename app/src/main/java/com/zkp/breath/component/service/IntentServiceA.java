d0fb0a19aa57e8afc845e8fb298ee73e731e77b8
zkp <1258520250@qq.com>
2020-03-01 16:41:33 +0800

    添加IntentService的例子

---
+++ app/src/main/java/com/zkp/breath/component/service/IntentServiceA.java
@@ -0,0 +1,28 @@
+package com.zkp.breath.component.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 内部核心HandlerThread(继承了线程Thread和内部创建了该线程的Looper)和ServiceHandler(接受HandlerThread创建的
+ * Looper用于处理消息的Handler)，外部携带的Intent用Message包裹然后发送给mServiceHandler，然后提供一个抽象方法
+ * onHandleIntent让用户接管处理消息（内部又把msg强转回Intent）
+ */
+public class IntentServiceA extends IntentService {
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public IntentServiceA(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
