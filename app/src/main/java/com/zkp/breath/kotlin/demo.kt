c29e485832c1fe42de681d493e45d00d4e6d9696
zkp <1258520250@qq.com>
2020-03-09 13:18:23 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/demo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/demo.kt
@@ -4,26 +4,30 @@ import android.util.Log
 
 /**
  * Created b Zwp on 2019/7/19.
+ *
+ * 在main方法中和android环境中执行的结果好像是不一样的.有时间做下测试
  */
 
 fun xxx() {
 
-    var z: Int = 128
-    var a: Int? = z
-    var b: Int? = a
-    Log.i("是否相等2", ": "+(a === b))
-
+    var z: Int = 128        // 调试后z对应java的int类型
+    var a: Int? = z         // a对应java的Integer类型
+    var b: Int? = a         // 把a的内存地址赋值给b引用
+    println("是否相等2:${a === b}")
+    // false;identity equality for arguments of types Int and Int? can be unstable beacause of implicit boxing
+    // 简单说就是拆箱和封箱是没办法进行内存比较的
+    println("是否相等2:${z === b}")
 
     var q: Int? = 128
     var w: Int? = 128
     var e: Int? = 128
-    Log.i("是否相等3", ": "+(w === e))
+    println("是否相等3:${w === e}")
 
 
     var i: Int? = 128
     var o: Int? = i
     var p: Int? = i
-    Log.i("是否相等4", ": "+(o === p))
+    println("是否相等4:${o === p}")
 }
 
 
