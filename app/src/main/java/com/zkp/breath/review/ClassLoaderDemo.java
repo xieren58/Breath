cd6b46528a218a1c3992aad97bb6bb7577086df8
zkp <1258520250@qq.com>
2020-03-09 18:24:25 +0800

    update

--- app/src/main/java/com/zkp/breath/review/ClassLoaderDemo.java
+++ app/src/main/java/com/zkp/breath/review/ClassLoaderDemo.java
@@ -25,9 +25,9 @@ public class ClassLoaderDemo {
             if(c != null){
                 try {
                     Object obj = c.newInstance();
-                    Method method = c.getDeclaredMethod("say",null);
+                    Method method = c.getDeclaredMethod("say", (Class<?>) null);
                     //通过反射调用Test类的say方法
-                    method.invoke(obj, null);
+                    method.invoke(obj, (Object) null);
                 } catch (InstantiationException | IllegalAccessException
                         | NoSuchMethodException
                         | SecurityException |
