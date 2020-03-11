1f0593c108a8de59b81a2df84e691aed97d5271c
zkp <1258520250@qq.com>
2020-03-11 16:33:33 +0800

    update

--- app/src/main/java/com/zkp/breath/designpattern/adapter/classadapter/ClassAdapter.java
+++ app/src/main/java/com/zkp/breath/designpattern/adapter/classadapter/ClassAdapter.java
@@ -4,6 +4,8 @@ import android.util.Log;
 
 /**
  * Created b Zwp on 2019/7/17.
+ * ITarget可以看成是我要的格式，而Adaptee是用户提供的格式。这样我们就能获取到我们指定的格式，但实际操作的是用户提供的数据
+ * 我们创建完对象后用ITarget去指向，但是内部内部调用Adaptee的方法。
  */
 public class ClassAdapter extends Adaptee implements ITarget {
 
