11474b702b7e71f985df4a12e4b3be52d35cd7f9
zkp <1258520250@qq.com>
2020-03-12 15:14:12 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/ArrayDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/ArrayDemo.kt
@@ -1,10 +1,11 @@
 package com.zkp.breath.kotlin
 
 /**
- * 数组
+ * 数组，即便有自动推断类型，最好还是指定其泛型类型或者使用指定类型的包装函数（intArrayOf）进行创建。这样才能
+ * 避免类型强转出错。
  */
 fun main() {
-    // 示例
+    // 可空类型
     val arrayOfNulls = arrayOfNulls<String>(3)
     // val arrayOfNulls: Array<String?> = arrayOfNulls(3) // 另外一种写法
     // 设置对应角标的值的不同写法
@@ -14,11 +15,14 @@ fun main() {
     arrayOfNulls.forEach { println("字符串数组内容: " + it) }
     println()
 
+    // 自动推断类型,不安全
     val arrayOf = arrayOf(1, 2, 3)
     arrayOf.forEach { println("int数组内容: " + it) }
+    // 指定类型
     val intArrayOf = intArrayOf(1, 2, 3)
     intArrayOf.forEach { println("无封箱int数组内容: " + it) }
     println()
+    // 指定类型，通过lambda表达式赋值
     val intArray = IntArray(5) { index -> index * 2 }
     intArray.forEach { print("无封箱int数组内容创建方式2：$it") }
 
