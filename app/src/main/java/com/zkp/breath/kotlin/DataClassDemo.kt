cae9a6fd91b741cf167851fff2cf7f2b83029186
zkp <1258520250@qq.com>
2020-03-13 21:52:08 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
@@ -1,5 +1,6 @@
 package com.zkp.breath.kotlin
 
+
 // 数据类，提供了一些常用的方法。对应java的Bean类
 // 自建函数componentN()，析构函数，copy函数
 data class DataClass(val s: String, var i: Int) {
@@ -18,16 +19,23 @@ fun main() {
     val (s, i) = copy1
     // 直接调用数据类对象其实内部调用是toString()方法
     println("copy1的toString(): ${copy1}")
-
     // 标准库提供的数据类
     val pair = Pair<String, String>("a", "b")
     val triple = Triple<String, String, String>("a", "b", "c")
+    // 下划线表示没有使用到的变量
     val (_, s2) = pair
     println("打印参数2：${s2}")
 
     // 解构方法
     val (i1, i2, i3) = resolve()
     println("i1: ${i1}, i2: ${i2}, i3: ${i3}")
+
+    // Map优雅的遍历方式
+    // 接收参数接收的是一个Pair，Pair本身也是Data类
+    val hashMapOf = hashMapOf("a" to 1, "b" to 2, "c" to 3)
+    for ((key, value) in hashMapOf) {
+        println("获取到的key:${key}，value是：${value}")
+    }
 
 }
 
