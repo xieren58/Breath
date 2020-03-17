4936e63da97240f5d8cb5259678d9e21458100b7
zkp <1258520250@qq.com>
2020-03-17 16:23:47 +0800

    update

--- app/src/main/java/com/zkp/breath/BaseApplication.java
+++ app/src/main/java/com/zkp/breath/BaseApplication.java
@@ -1,6 +1,10 @@
 package com.zkp.breath;
 
+import android.os.StrictMode;
+
 import androidx.multidex.MultiDexApplication;
+
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created b Zwp on 2019/7/25.
@@ -12,5 +16,18 @@ public class BaseApplication extends MultiDexApplication {
         super.onCreate();
         // 常用工具库初始化
         com.blankj.utilcode.util.Utils.init(this);
+        setupLeakCanary();
     }
+
+    protected void setupLeakCanary() {
+        // 判断是否是 HeapAnalyzerService 所属进程
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        // 注册 LeakCanary
+        LeakCanary.install(this);
+    }
+
 }
