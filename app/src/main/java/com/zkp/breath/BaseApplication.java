7397f5fe969c1ce04c23a2bfda1bcc97326c9781
zkp <1258520250@qq.com>
2020-03-20 15:29:14 +0800

    添加界面卡顿检查工具

--- app/src/main/java/com/zkp/breath/BaseApplication.java
+++ app/src/main/java/com/zkp/breath/BaseApplication.java
@@ -3,6 +3,9 @@ package com.zkp.breath;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.github.moduth.blockcanary.BlockCanary;
+import com.github.moduth.blockcanary.BlockCanaryContext;
+
 
 /**
  * Created b Zwp on 2019/7/25.
@@ -14,6 +17,8 @@ public class BaseApplication extends MultiDexApplication {
         super.onCreate();
         // 常用工具库初始化
         com.blankj.utilcode.util.Utils.init(this);
+        // 初始化界面卡顿检查工具
+        BlockCanary.install(this, new BlockCanaryContext()).start();
     }
 
 }
