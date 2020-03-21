be7cfccf57d9414d18ff2538b020807a9851cff2
zkp <1258520250@qq.com>
2020-03-21 22:57:57 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/base/BaseAppCompatActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/base/BaseAppCompatActivity.kt
@@ -1,5 +1,6 @@
 package com.zkp.breath.component.activity.base
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,12 +8,24 @@ import com.blankj.utilcode.util.BarUtils
 
 abstract class BaseAppCompatActivity : AppCompatActivity() {
 
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 隐藏标题栏
         supportActionBar?.hide()
         // 隐藏状态栏
         BarUtils.setStatusBarVisibility(this, false)
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
     }
 
     //应用的启动图标在桌面上的位置有变化，可在此收到新的位置信息，
