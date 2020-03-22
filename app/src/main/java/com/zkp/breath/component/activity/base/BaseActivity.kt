8c477fc17621494ed60b47924eeb988c09e34124
zkp <1258520250@qq.com>
2020-03-22 23:57:10 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/base/BaseActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/base/BaseActivity.kt
@@ -3,42 +3,69 @@ package com.zkp.breath.component.activity.base
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.blankj.utilcode.util.BarUtils
 import me.jessyan.autosize.AutoSizeConfig
 
 abstract class BaseActivity : AppCompatActivity() {
 
+    val TAG = this::class.simpleName
+
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+        Log.i(TAG, "onAttachedToWindow()")
     }
 
+    /**
+     * 在这个方法中会创建PhoneWindow，WindowManagerImpl,DecorView(把setContentView中的布局文件添加到其ContentView中)
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         //使用设备的完整尺寸(即隐藏标题栏和状态栏)
         AutoSizeConfig.getInstance().isUseDeviceSize = true
         super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate(savedInstanceState: Bundle?)")
+//        hideTitleBarAndStateBar()
+    }
+
+    /**
+     * 隐藏状态栏和标题栏
+     */
+    private fun hideTitleBarAndStateBar() {
         // 隐藏标题栏
-//        supportActionBar?.hide()
+        supportActionBar?.hide()
         // 隐藏状态栏
-//        BarUtils.setStatusBarVisibility(this, false)
+        BarUtils.setStatusBarVisibility(this, false)
+    }
+
+    /**
+     * 通过ViewRootImpl将DecorView和PhoneWindow进行绑定,正真触发绘制流程。
+     */
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume()")
     }
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(newBase)
+        Log.i(TAG, "attachBaseContext(newBase: Context?)")
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
+        Log.i(TAG, "onDetachedFromWindow()")
     }
 
     //应用的启动图标在桌面上的位置有变化，可在此收到新的位置信息，
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        this.intent = intent
+        Log.i(TAG, "onNewIntent(intent: Intent?)")
     }
 
     // 当setContentView设置显示后会回调Activity的onContentChanged方法
     override fun onContentChanged() {
         super.onContentChanged()
+        Log.i(TAG, "onContentChanged()")
     }
 
 }
