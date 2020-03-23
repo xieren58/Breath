b614f5b8033bdc1b68849c9e00ab02cc17e3c594
zkp <1258520250@qq.com>
2020-03-23 18:54:01 +0800

    添加进程判断，防止资源重复初始化

--- app/src/main/java/com/zkp/breath/BaseApplication.java
+++ app/src/main/java/com/zkp/breath/BaseApplication.java
@@ -3,6 +3,7 @@ package com.zkp.breath;
 
 import androidx.multidex.MultiDexApplication;
 
+import com.blankj.utilcode.util.ProcessUtils;
 import com.github.moduth.blockcanary.BlockCanary;
 import com.github.moduth.blockcanary.BlockCanaryContext;
 
@@ -15,10 +16,12 @@ public class BaseApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        // 常用工具库初始化
-        com.blankj.utilcode.util.Utils.init(this);
-        // 初始化界面卡顿检查工具
-        BlockCanary.install(this, new BlockCanaryContext()).start();
+        if (ProcessUtils.isMainProcess()) {
+            // 常用工具库初始化
+            com.blankj.utilcode.util.Utils.init(this);
+            // 初始化界面卡顿检查工具
+            BlockCanary.install(this, new BlockCanaryContext()).start();
+        }
     }
 
 }
