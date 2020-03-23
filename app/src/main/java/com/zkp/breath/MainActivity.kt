00a0c544727f55ab0a993f7d87776c1cce44d8a6
zkp <1258520250@qq.com>
2020-03-23 19:13:50 +0800

    convert kotlin

---
+++ app/src/main/java/com/zkp/breath/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.zkp.breath
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import com.blankj.utilcode.util.ToastUtils
+import com.zkp.breath.component.activity.base.BaseActivity
+import com.zkp.breath.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
+        //        ActivityUtils.startActivity(ActivityCoordinator.class);
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    //  保存点击的时间
+    private var exitTime: Long = 0
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (System.currentTimeMillis() - exitTime > 2000) {
+                ToastUtils.showShort("再按一次退出程序")
+                exitTime = System.currentTimeMillis()
+            } else {
+                System.exit(0)
+            }
+            return false
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
