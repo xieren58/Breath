c06e15d5d104422325f73bb167b86b6e066a2673
zkp <1258520250@qq.com>
2020-03-27 18:02:41 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/ExtensionMethodDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/ExtensionMethodDemo.kt
@@ -154,8 +154,7 @@ class C4 : C3() {
 
 fun String?.toString(): String {
     if (this == null) return "null"
-    // 空检测之后，“this”会自动转换为非空类型，所以下面的 toString()
-    // 解析为 Any 类的成员函数
+    // 空检测之后，“this”会自动转换为非空类型，调用不需要添加?.
     return toString()
 }
 
