c7a228b66eaf47e40d51683f8d5a10986c29290d
zkp <1258520250@qq.com>
2020-03-30 16:13:35 +0800

    update

--- app/src/main/java/com/zkp/breath/BaseApplication.java
+++ app/src/main/java/com/zkp/breath/BaseApplication.java
@@ -24,19 +24,4 @@ public class BaseApplication extends MultiDexApplication {
         }
     }
 
-//    private void initTinker() {
-//        // 我们可以从这里获得Tinker加载过程的信息
-//        tinkerApplicationLike = TinkerPatchApplicationLike.getTinkerPatchApplicationLike();
-//
-//        // 初始化TinkerPatch SDK, 更多配置可参照API章节中的,初始化SDK
-//        TinkerPatch.init(tinkerApplicationLike)
-//                .reflectPatchLibrary()
-//                .setPatchRollbackOnScreenOff(true)
-//                .setPatchRestartOnSrceenOff(true)
-//                .setFetchPatchIntervalByHours(3);
-//
-//        // 每隔3个小时(通过setFetchPatchIntervalByHours设置)去访问后台时候有更新,通过handler实现轮训的效果
-//        TinkerPatch.with().fetchPatchUpdateAndPollWithInterval();
-//    }
-
 }
