47b379e60baa1ad3aa7c93849479e52a83f90d9b
zkp <1258520250@qq.com>
2020-03-30 23:10:10 +0800

    添加viewpager的例子

---
+++ app/src/main/java/com/zkp/breath/adpter/ViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.zkp.breath.adpter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.zkp.breath.R;
+
+import java.util.List;
+import java.util.Random;
+
+public class ViewPagerAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
+
+    private int[] colors = new int[]{0xFF008577, 0xFF03A9F4, 0xFFD81B60, 0xFFFF9800, 0xFF4CAF50, 0xFF673AB7};
+
+    public ViewPagerAdapter(@Nullable List<String> data) {
+        // 子view一定要match_parent
+        // java.lang.IllegalStateException: Pages must fill the whole ViewPager2 (use match_parent)
+        super(R.layout.adpter_viewpager2, data);
+    }
+
+    @Override
+    protected void convert(@NonNull BaseViewHolder helper, String item) {
+        Random random = new Random();
+        int i = random.nextInt(colors.length);
+        helper.setText(R.id.tv, item);
+        View convertView = helper.getConvertView();
+        convertView.setBackgroundColor(colors[i]);
+    }
+}
