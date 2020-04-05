3f133d3a8f4e56baa858c7da09e1126b55ef4ef0
zkp <1258520250@qq.com>
2020-04-05 20:24:29 +0800

    update

--- app/src/main/java/com/zkp/breath/arouter/interfaces/HelloService.kt
+++ app/src/main/java/com/zkp/breath/arouter/interfaces/HelloService.kt
@@ -4,7 +4,7 @@ import com.alibaba.android.arouter.facade.template.IProvider
 
 /**
  * 这里的service是一种面向接口开发的概念，该接口必须继承IProvider。
- * 其实就是一个普通类，在不同模块且无依赖的情况下通过这种形式能被调用，其实就和跳转activity一样。
+ * 其实就是一个普通类，通过实现注解的方式实例化
  */
 interface HelloService : IProvider {
     fun sayHello(name: String?): String?
