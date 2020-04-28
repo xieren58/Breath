d54e29d64ca2cc17ba80a9fa90e1b4496e8fb2c5
zkp <1258520250@qq.com>
2020-04-28 22:02:35 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/ArrayDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/ArrayDemo.kt
@@ -1,28 +1,40 @@
 package com.zkp.breath.kotlin
 
 /**
- * Kotlin 中的数组是一个拥有泛型的类，相对于java来说就拥有了和集合一样的功能。
+ * 1.Kotlin 中的数组是一个拥有泛型的类，相对于java来说就拥有了和集合一样的功能。
+ * 2.Kotlin 的数组编译成字节码时使用的仍然是 Java 的数组，但在语言层面是泛型实现，这样会失去协变 (covariance) 特性，
+ * 就是子类数组对象不能赋值给父类的数组变量（多态）：
+ *
  * 数组，即便有自动推断类型，最好还是指定其泛型类型或者使用非包装类型的函数（intArrayOf）进行创建。这样才能
  * 避免类型强转出错。
  */
 fun main() {
+
+    // kotlin的数组语言层面支持泛型，而泛型不支持协变。
+    val strs: Array<String> = arrayOf("a", "b", "c")
+//    val anys: Array<Any> = strs // compile-error: Type mismatch，
+
     // 可空类型
     val arrayOfNulls = arrayOfNulls<String>(3)
-    // val arrayOfNulls: Array<String?> = arrayOfNulls(3) // 另外一种写法
+    val arrayOfNulls2: Array<String?> = arrayOfNulls(3) // 另外一种写法
+
     // 设置对应角标的值的不同写法
     arrayOfNulls.set(0, "a")
     arrayOfNulls[1] = "b"   // 重置了get/set方法
     arrayOfNulls[2] = "c"
+
+    // 遍历
     arrayOfNulls.forEach { println("字符串数组内容: " + it) }
     println()
 
-    // 自动推断类型,不安全
+    // 自动推断类型
     val arrayOf = arrayOf(1, 2, 3)
     arrayOf.forEach { println("int数组内容: " + it) }
+
     // 指定类型
     val intArrayOf = intArrayOf(1, 2, 3)
     intArrayOf.forEach { println("无封箱int数组内容: " + it) }
-    println()
+
     // 指定类型，通过lambda表达式赋值
     val intArray = IntArray(5) { index -> index * 2 }
     intArray.forEach { print("无封箱int数组内容创建方式2：$it") }
@@ -31,7 +43,6 @@ fun main() {
     // index表示角标
     val asc = Array(5, { index -> (index * index).toString() })
     asc.forEach { println(it) }
-    println()
 
 }
 
