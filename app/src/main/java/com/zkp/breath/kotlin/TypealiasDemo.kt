0758bcdf62eaed0985344a80623f5c73bc698f66
zwp <1258520250@qq.com>
2020-05-07 18:22:59 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/TypealiasDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/TypealiasDemo.kt
@@ -23,6 +23,7 @@ class MyType<U, T>(a: U, b: T) {
 // val s: MyType<String, Int> = MyType("", 1)   // 声明比较长
 // val s1 = MyType("", 1)   // 自动推断类型，推荐写法
 typealias UType = MyType<String, Int>
+
 // 剩下一个泛型类型给用户指定
 typealias TType<U> = MyType<U, Int>
 
@@ -44,16 +45,30 @@ typealias t3 = t1<String>
 
 fun fooT1(t: t1<Int>) {
 }
+
 fun fooT2(t: t2) {
 }
+
 fun fooT3(t: t3) {
 }
 
 
 // 参数为泛型的函数类型使用别名
 typealias methodTT<U, T> = (U, T) -> String
+
 fun <U, T> methodT(f: methodTT<U, T>, a: U, b: T) {
     f(a, b)
+}
+
+
+fun main() {
+    // 类型别名声明的变量的泛形不能省略
+    val s: methodTT<String, String> = { s1, s2 -> s1 + s2 }
+    val s1: methodTT<String, String> = { s1: String, s2: String -> s1 + s2 }
+
+    methodT(s, "1", "2")
+    methodT({ i1, i2 -> i1 + i2 }, "1", "2")
+    methodT<String, String>({ i1, i2 -> i1 + i2 }, "1", "2")
 }
 
 
