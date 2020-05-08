643a5bec83557c1b9c890f417cc935f94c1860c3
zwp <1258520250@qq.com>
2020-05-08 20:57:25 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/MethodDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/MethodDemo.kt
@@ -96,11 +96,25 @@ class MethodClass {
     // lambda 表达式，即函数类型的字面量（看例子的调用方式就知道）
     // 函数类型为可null类型，返回值为可null类型
     fun <T> lock2(t: Int, body: (() -> T)?): T? {
+
+        // 方法一：比较常见的写法
+        // 因为函数类型为可null类型所以需要判空
         if (body != null) {
             return body()
         }
+
+        // 方法二
+        // 使用？.操作符，如果不为null则调用let函数，let函数内的it表示body，所以需要在it后面添加()表示调用自己。
+        body?.let { it() }
+
+
+        // invoke方法，只有函数
+        body?.invoke()
+
         return null
     }
+
+
 
     fun <T, R> List<T>.map(transform: (T) -> R): List<R> {
         val result = arrayListOf<R>()
@@ -138,77 +152,101 @@ fun dfs(s: String, i: Int) {
     dfs(1)
 }
 
+
+
+fun <T> lock3(body: (() -> T)?): T? {
+    // 1
+//    if (body != null) {
+//        return body()
+//    }
+//    return null
+
+    // 2
+//    return body?.invoke()
+
+    // 3
+    return body?.let { it() }
+}
+
+
 fun main() {
 
-    val methodClass = MethodClass()
+    val lock3 = lock3 { "坑爹的逻辑" }
+    println("ssss : $lock3")
 
-    methodClass.lambad1 { s, s1 -> s + s1 }     // 知道了类型，可以省略,切记参数不用加括号！！！！
-    methodClass.lambad1 { s: Int, s1: Int -> s + s1 }     // 最完整的写法，类型也写上，切记参数不用加括号！！！！
-    methodClass.lambad2 { (s, s1) -> s + s1 }   // 知道了类型，可以省略。注意这里加了圆括号，因为Pair有组建函数ComponetN,所以这里是解构声明的写法
-    methodClass.lambad2 { (s, s1): Pair<Int, Int> -> s + s1 }   // 最完整的写法，类型也写上。注
-
-    val body = { "我们" }
-    val filter = methodClass.filter(body)
-    val filter1 = methodClass.filter { "我们" }
-    println(filter)
-
-    methodClass.filter1("", { str -> str + "扩展的部分" })
-    methodClass.filter1(1, { i -> i + 2 })
-    val filter1Body: (Int) -> Int = { i -> i + 2 }
-    methodClass.filter1(1, filter1Body)
-
-    val filter1Body2: (String, String) -> String = { str1, str2 -> str1 + str2 }
-    methodClass.filter1("", { str1, str2 -> str1 + str2 })
-    methodClass.filter1("", filter1Body2)
+    val lock4 = lock3 { null }
+    println("ssss : $lock4")
 
 
-    val xxww1 = methodClass.filter {
-        val s: String = "我们"
-        val s1: String = "你们"
-        s + s1
-    }
-    println(xxww1)
-
-    val xxww2 = methodClass.filter {
-        val s: String = "我们"
-        val s1: String = "你们"
-        // 返回值，作用的范围为filter方法，否则会结束main方法。
-        return@filter s + s1
-    }
-    println(xxww2)
-
-    methodClass.filter2<String>("我们") { s -> s + "拼接字段" }
-
-
-    val lock = methodClass.lock<String>(1) { i -> "hello to myself:$i" }
-
-
-    // 返回类型为可null，能够自动推出类型
-    val bodyLock2: String? = methodClass.lock2(1, { "我们" })
-    // 传入函数参数null.
-    val lock22 = methodClass.lock2<String>(1, null)
-    // 函数类型声明，函数类型字面量。
-    val body1: (() -> String)? = null
-    val body2: (() -> String)? = { "我们" }
-    val lock2 = methodClass.lock2(1, body1)
-    val lock21 = methodClass.lock2(1, body2)
-    val lock23 = methodClass.lock2(1) { "我们" }
-    val lock24 = methodClass.lock2(1) { null }
-    println()
-
-    arrayOf(3, 4, 5).filter { s -> s > 0 }
-    // 引用方式1, ”::"一元操作符要写在函数名前面，圆括号包裹
-    arrayOf(3, 4, 5).filter(::isEven)
-    // 引用方式2，花阔号包裹
-    arrayOf(3, 4, 5).filter { isEven(it) }
-    println()
-
-    arrayOf("你", "我", "ta").filter { it.equals("你") }
-    arrayOf("你", "我", "ta").filter("你"::equals)
-    println()
-
-    comboTwoValue(3, 4, Math::min)
-    comboTwoValue(3, 4, { a, b -> Math.min(a, b) })
-    println()
+//    val methodClass = MethodClass()
+//
+//    methodClass.lambad1 { s, s1 -> s + s1 }     // 知道了类型，可以省略,切记参数不用加括号！！！！
+//    methodClass.lambad1 { s: Int, s1: Int -> s + s1 }     // 最完整的写法，类型也写上，切记参数不用加括号！！！！
+//    methodClass.lambad2 { (s, s1) -> s + s1 }   // 知道了类型，可以省略。注意这里加了圆括号，因为Pair有组建函数ComponetN,所以这里是解构声明的写法
+//    methodClass.lambad2 { (s, s1): Pair<Int, Int> -> s + s1 }   // 最完整的写法，类型也写上。注
+//
+//    val body = { "我们" }
+//    val filter = methodClass.filter(body)
+//    val filter1 = methodClass.filter { "我们" }
+//    println(filter)
+//
+//    methodClass.filter1("", { str -> str + "扩展的部分" })
+//    methodClass.filter1(1, { i -> i + 2 })
+//    val filter1Body: (Int) -> Int = { i -> i + 2 }
+//    methodClass.filter1(1, filter1Body)
+//
+//    val filter1Body2: (String, String) -> String = { str1, str2 -> str1 + str2 }
+//    methodClass.filter1("", { str1, str2 -> str1 + str2 })
+//    methodClass.filter1("", filter1Body2)
+//
+//
+//    val xxww1 = methodClass.filter {
+//        val s: String = "我们"
+//        val s1: String = "你们"
+//        s + s1
+//    }
+//    println(xxww1)
+//
+//    val xxww2 = methodClass.filter {
+//        val s: String = "我们"
+//        val s1: String = "你们"
+//        // 返回值，作用的范围为filter方法，否则会结束main方法。
+//        return@filter s + s1
+//    }
+//    println(xxww2)
+//
+//    methodClass.filter2<String>("我们") { s -> s + "拼接字段" }
+//
+//
+//    val lock = methodClass.lock<String>(1) { i -> "hello to myself:$i" }
+//
+//
+//    // 返回类型为可null，能够自动推出类型
+//    val bodyLock2: String? = methodClass.lock2(1, { "我们" })
+//    // 传入函数参数null.
+//    val lock22 = methodClass.lock2<String>(1, null)
+//    // 函数类型声明，函数类型字面量。
+//    val body1: (() -> String)? = null
+//    val body2: (() -> String)? = { "我们" }
+//    val lock2 = methodClass.lock2(1, body1)
+//    val lock21 = methodClass.lock2(1, body2)
+//    val lock23 = methodClass.lock2(1) { "我们" }
+//    val lock24 = methodClass.lock2(1) { null }
+//    println()
+//
+//    arrayOf(3, 4, 5).filter { s -> s > 0 }
+//    // 引用方式1, ”::"一元操作符要写在函数名前面，圆括号包裹
+//    arrayOf(3, 4, 5).filter(::isEven)
+//    // 引用方式2，花阔号包裹
+//    arrayOf(3, 4, 5).filter { isEven(it) }
+//    println()
+//
+//    arrayOf("你", "我", "ta").filter { it.equals("你") }
+//    arrayOf("你", "我", "ta").filter("你"::equals)
+//    println()
+//
+//    comboTwoValue(3, 4, Math::min)
+//    comboTwoValue(3, 4, { a, b -> Math.min(a, b) })
+//    println()
 
 }
