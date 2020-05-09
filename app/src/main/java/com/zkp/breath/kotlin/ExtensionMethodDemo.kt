4e6ebd5738badf19de9b4965d945a5e7bbeb3901
zwp <1258520250@qq.com>
2020-05-09 18:02:06 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/ExtensionMethodDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/ExtensionMethodDemo.kt
@@ -170,7 +170,7 @@ fun String?.toString(): String {
 // 扩展属性
 // 扩展属性允许定义在类或者kotlin文件中，不允许定义在函数中，扩展属性只能被声明为 val,扩展属性不能有初始化器，没有后端字段field。
 // 不能有初始化是因为扩展的前提是当前类存在，而这个属性应该指向其他资源，如果扩展属性有初始器那么就表明这个对象早已被创建。
-val <T> List<T>.lastIndex: Int
+val <T> List<T>.cusLastIndex: Int
     get() = size
 
 // ======================================================
@@ -326,4 +326,7 @@ fun main(args: Array<String>) {
     // 所以这里调用的是顶层伴生对象扩展函数
     MyClass.foo()
     MyClass.companionFun2()
+
+
+    val cusLastIndex = arrayListOf<String>("1", "2").cusLastIndex
 }
