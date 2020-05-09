6dad57afb7ab7fde1051ec751dcee2b6dc57fd4c
zwp <1258520250@qq.com>
2020-05-09 16:54:59 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/ForDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/ForDemo.kt
@@ -4,20 +4,22 @@ fun main() {
 
     val intArrayOf = intArrayOf(1, 2, 3)
 
+
     // for 循环可以对任何提供迭代器（iterator）的对象进行遍历，语法如下:
+    // for (变量名 【: 类型】 in  遍历对象)，其中类型部分可省略
     for (item in intArrayOf) print(item)
+//    for (item: Int in intArrayOf)
 
-// 循环体可以是一个代码块:
-    for (item: Int in intArrayOf) {
-        // ……
-    }
 
-// 如果你想要通过索引遍历一个数组或者一个 list，你可以这么做：
+    // 如果你想要通过索引遍历一个数组或者一个 list，语法如下：
+    // for(角标名 【: 类型】 in  遍历对象.indices)，其中类型部分可省略
+//        for(i :Int in intArrayOf.indices)
     for (i in intArrayOf.indices) {
         print(intArrayOf[i])
     }
 
-// 获取角标和值
+    // 获取角标和值
+    // for ((index: Int, value: 遍历对象的元素类型) in intArrayOf.withIndex()) {
     for ((index, value) in intArrayOf.withIndex()) {
         println("the element at $index is $value")
     }
@@ -34,12 +36,12 @@ fun main() {
         println(x)
     }
 
-// 使用 step 指定步长
+    // 使用 step 指定步长
     for (i in 1..4 step 2) print(i) // 输出“13”
 
     for (i in 4 downTo 1 step 2) print(i) // 输出“42”，
 
-// 使用 until 函数排除结束元素
+    // 使用 until 函数排除结束元素
     for (i in 1 until 10) {   // i in [1, 10) 排除了 10
         println(i)
     }
