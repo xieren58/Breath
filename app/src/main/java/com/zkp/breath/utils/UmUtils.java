d1daf171a807a3b78b4ab51e00fe2c8718c4bdb9
zwp <1258520250@qq.com>
2020-05-11 20:52:47 +0800

    update

--- app/src/main/java/com/zkp/breath/utils/UmUtils.java
+++ app/src/main/java/com/zkp/breath/utils/UmUtils.java
@@ -1,8 +1,11 @@
 package com.zkp.breath.utils;
 
+import android.app.Notification;
 import android.content.Context;
+import android.widget.RemoteViews;
 
 import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.NotificationUtils;
 import com.blankj.utilcode.util.ProcessUtils;
 import com.blankj.utilcode.util.ThreadUtils;
 import com.umeng.analytics.MobclickAgent;
@@ -10,7 +13,10 @@ import com.umeng.commonsdk.UMConfigure;
 import com.umeng.commonsdk.statistics.common.DeviceConfig;
 import com.umeng.message.IUmengRegisterCallback;
 import com.umeng.message.PushAgent;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.entity.UMessage;
 import com.zkp.breath.BuildConfig;
+import com.zkp.breath.R;
 
 import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
@@ -56,6 +62,10 @@ public class UmUtils {
             UMConfigure.init(context, UMConfigure.DEVICE_TYPE_PHONE, "765597c10774728c396403cbf461ff6a");
             // 推送流程
             PushAgent mPushAgent = PushAgent.getInstance(context);
+            // 自定义通知栏是否显示
+//            mPushAgent.setNotificaitonOnForeground(false);
+            // 自定义样式配置
+            mPushAgent.setMessageHandler(customNotification());
             //注册推送服务，每次调用register方法都会回调该接口
             mPushAgent.register(new IUmengRegisterCallback() {
                 @Override
@@ -70,6 +80,36 @@ public class UmUtils {
                 }
             });
         });
+    }
+
+    private static UmengMessageHandler customNotification() {
+        UmengMessageHandler messageHandler = new UmengMessageHandler() {
+            @Override
+            public Notification getNotification(Context context, UMessage msg) {
+                switch (msg.builder_id) {
+//                    case 1:
+//                        Notification.Builder builder = new Notification.Builder(context);
+//                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(),
+//                                R.layout.notification_view);
+//                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+//                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+//                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon,
+//                                getLargeIcon(context, msg));
+//                        myNotificationView.setImageViewResource(R.id.notification_small_icon,
+//                                getSmallIconId(context, msg));
+//                        builder.setContent(myNotificationView)
+//                                .setSmallIcon(getSmallIconId(context, msg))
+//                                .setTicker(msg.ticker)
+//                                .setAutoCancel(true);
+//                        return builder.getNotification();
+                    default:
+                        //默认为0，若填写的builder_id并不存在，也使用默认。
+                        return super.getNotification(context, msg);
+                }
+            }
+        };
+
+        return messageHandler;
     }
 
     private static String[] getTestDeviceInfo(Context context) {
