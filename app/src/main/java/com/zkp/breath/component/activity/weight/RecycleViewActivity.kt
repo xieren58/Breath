52967ed5a2cadcd757104ba4448c0970c708ec65
zwp <1258520250@qq.com>
2020-05-13 14:55:39 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/weight/RecycleViewActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/weight/RecycleViewActivity.kt
@@ -78,13 +78,18 @@ class RecycleViewActivity : BaseActivity() {
         }
 
         val rcv = binding.rcv
-        rcv.itemAnimator?.changeDuration = 0
+        /**
+         * 取消动画
+         * 3.0.2版本，开启了loadmore功能，也开启DiffUtils功能，使用setDiffNewData方法会自动滑倒底部，需要加上
+         * 下面的方法才能避免，已经提交github的issues。（#3177）
+         */
         rcv.itemAnimator = null
         rcv.overScrollMode = View.OVER_SCROLL_NEVER
         val staggeredGridLayoutManager = LinearLayoutManager(this)
         rcv.layoutManager = staggeredGridLayoutManager
         val gridAdapter = LoadMoreAdapter(arrayList)
         gridAdapter.setOnItemClickListener(onItemClickListener)
+
         // 动画
         gridAdapter.animationEnable = true
         gridAdapter.isAnimationFirstOnly = false
@@ -103,33 +108,53 @@ class RecycleViewActivity : BaseActivity() {
         // 获取模块
         val loadMoreModule = gridAdapter.loadMoreModule
         // 打开或关闭加载更多功能（默认为true）
-        loadMoreModule.isEnableLoadMore = false
+        loadMoreModule.isEnableLoadMore = true
         // 所有数据加载完成后，是否允许点击（默认为false）
         loadMoreModule.enableLoadMoreEndClick = true
         // 是否自定加载下一页（默认为true）
-//        loadMoreModule.isAutoLoadMore = false
+        loadMoreModule.isAutoLoadMore = true
         // 预加载的位置（默认为1）。  一般用于分页加载，一般设置请求数据量的一半即可。
-//        loadMoreModule.preLoadNumber = 5
+        loadMoreModule.preLoadNumber = 5
 
-//        loadMoreModule.setOnLoadMoreListener {
-//
-//            GlobalScope.launch(Dispatchers.Main) {
-//
-//                ToastUtils.showShort("进入load_more回调")
-//
-//                // 模拟load数据的过程
-//                withContext(Dispatchers.IO) {
-//                    Thread.sleep(3000)
-//                    Log.i(TAG, "launch_IO1: ${Thread.currentThread().name}")
-//                }
-//
-//                when (reqCount) {
-//                    // 当前这次数据加载错误，调用此方法
-//                    0 -> {
-//                        loadMoreModule.loadMoreFail()
-//                        reqCount++
-//                    }
-//                    1 -> {
+        loadMoreModule.setOnLoadMoreListener {
+
+            GlobalScope.launch(Dispatchers.Main) {
+
+                ToastUtils.showShort("进入load_more回调")
+
+                // 模拟load数据的过程
+                withContext(Dispatchers.IO) {
+                    Thread.sleep(3000)
+                    Log.i(TAG, "launch_IO1: ${Thread.currentThread().name}")
+                }
+
+                when (reqCount) {
+                    // 当前这次数据加载错误，调用此方法
+                    0 -> {
+                        loadMoreModule.loadMoreFail()
+                        reqCount++
+                    }
+                    1 -> {
+                        val newData = ArrayList<String>()
+                        for (i in 0..10) {
+                            newData.add("我是新load的数据".plus(i.toString()))
+                        }
+                        // 添加新load的模拟数据
+                        gridAdapter.addData(newData)
+                        // 当前这次数据加载完毕，调用此方法
+                        loadMoreModule.loadMoreComplete()
+
+                        reqCount++
+                    }
+
+                    2 -> {
+                        // 所有数据加载完成，调用此方法
+                        // 需要重置"加载完成"状态时，请调用 setNewData()
+                        loadMoreModule.loadMoreEnd()
+                    }
+
+                    // 上面的预加载功能打开才放开这段代码进行测试，上面的when的其余判断条件也要注释
+//                    else -> {
 //                        val newData = ArrayList<String>()
 //                        for (i in 0..10) {
 //                            newData.add("我是新load的数据".plus(i.toString()))
@@ -141,30 +166,10 @@ class RecycleViewActivity : BaseActivity() {
 //
 //                        reqCount++
 //                    }
-//
-//                    2 -> {
-//                        // 所有数据加载完成，调用此方法
-//                        // 需要重置"加载完成"状态时，请调用 setNewData()
-//                        loadMoreModule.loadMoreEnd()
-//                    }
-//
-//                    // 上面的预加载功能打开才放开这段代码进行测试，上面的when的其余判断条件也要注释
-////                    else -> {
-////                        val newData = ArrayList<String>()
-////                        for (i in 0..10) {
-////                            newData.add("我是新load的数据".plus(i.toString()))
-////                        }
-////                        // 添加新load的模拟数据
-////                        gridAdapter.addData(newData)
-////                        // 当前这次数据加载完毕，调用此方法
-////                        loadMoreModule.loadMoreComplete()
-////
-////                        reqCount++
-////                    }
-//                }
-//            }
-//
-//        }
+                }
+            }
+
+        }
     }
 
     private val onItemClickListener = OnItemClickListener { adapter, view, position ->
@@ -190,7 +195,7 @@ class RecycleViewActivity : BaseActivity() {
                 for (i in 0..10) {
                     arrayList.add(i.toString().plus("x"))
                 }
-                loadMoreAdapter?.setNewInstance(arrayList)
+                loadMoreAdapter?.setDiffNewData(arrayList)
 
             }
         } else if (position in 5..10) {
