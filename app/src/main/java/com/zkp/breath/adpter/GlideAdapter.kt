6b0b4c608da171640d6500c9ecb371a524f3d5b6
zwp <1258520250@qq.com>
2020-05-15 15:27:34 +0800

    update

--- app/src/main/java/com/zkp/breath/adpter/GlideAdapter.kt
+++ app/src/main/java/com/zkp/breath/adpter/GlideAdapter.kt
@@ -1,5 +1,6 @@
 package com.zkp.breath.adpter
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -7,6 +8,8 @@ import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomViewTarget
+import com.bumptech.glide.request.transition.Transition
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.zkp.breath.R
@@ -33,13 +36,17 @@ class GlideAdapter(data: MutableList<String>? = null) :
                 .transform(CenterCrop(), GranularRoundedCorners(100f, 100f, 0f, 0f))
                 // 禁止图片变换(取消之前设置的变化效果)
                 .dontTransform()
-                // 是否仅从缓存加载图片，如果缓存中没有，就会加载失败。
-//                .onlyRetrieveFromCache(false)
+                // 是否仅从缓存加载图片，如果缓存中没有，就会加载失败，不管有没有网络，一般这功能不开启，体验不好。
+//                .onlyRetrieveFromCache(true)
                 // 指定加载的图片大小，内部会按照最短边和源数据的宽高比进行计算
                 .override(300, 300)
 
 
         Glide.with(imageView.context)
+                /**
+                 * 默认返回Drawable类型的RequestBuilder，可以使用asBitmap返回对应资源类型的Bitmap类型的RequestBuilder，
+                 * 还有asGif,asFile等。
+                 */
                 .load(item)
                 /**
                  * 缩略图加载，其实就是先加载显示一张低分辨率的图片，再显示高分辨率，提供更好的视觉过度效果，
@@ -51,12 +58,18 @@ class GlideAdapter(data: MutableList<String>? = null) :
                  *
                  */
                 .thumbnail(0.25f)
-                // 交叉淡入变换
+                /**
+                 * 交叉淡入变换，如果是bitmap则使用BitmapTransitionOptions
+                 */
                 .transition(DrawableTransitionOptions.withCrossFade())
+                /**
+                 * apply() 方法可以被调用多次，因此 RequestOption 可以被组合使用，如果 RequestOptions
+                 * 对象之间存在相互冲突的设置，那么只有最后一个被应用的 RequestOptions 会生效。
+                 */
                 .apply(sharedOptions)
                 .into(imageView)
 
-        // 如果需要操控到源数据可以使用tager
+        // 如果需要操控到源数据可以使用target
 //        Glide.with(imageView.context)
 //                .asBitmap()
 //                .load(item)
