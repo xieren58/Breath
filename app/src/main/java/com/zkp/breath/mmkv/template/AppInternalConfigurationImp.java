bfca09c5bf94765fc59fef432a37efb2fb3e4027
zwp <1258520250@qq.com>
2020-05-16 10:35:34 +0800

    update

--- app/src/main/java/com/zkp/breath/mmkv/template/AppInternalConfigurationImp.java
+++ app/src/main/java/com/zkp/breath/mmkv/template/AppInternalConfigurationImp.java
@@ -5,9 +5,13 @@ import com.tencent.mmkv.MMKV;
 public class AppInternalConfigurationImp implements AppInternalConfiguration {
 
     private final MMKV mmkv;
+    private static final String ENCRYPT_KEY = AppInternalConfigurationImp.class.getSimpleName();
 
     public AppInternalConfigurationImp() {
-        mmkv = MMKV.mmkvWithID(ConfigurationField.ConfigurationFile.INTERNAL_CONFIGURATION);
+        mmkv = MMKV.mmkvWithID(ConfigurationField.ConfigurationFile.INTERNAL_CONFIGURATION,
+                MMKV.SINGLE_PROCESS_MODE, ENCRYPT_KEY);
+        // 取消加密
+        mmkv.reKey(null);
     }
 
     @Override
