5b72925905707e7269533c09ca21471adcef64e3
zwp <1258520250@qq.com>
2020-05-18 11:46:08 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/AccessModifier.kt
+++ app/src/main/java/com/zkp/breath/kotlin/AccessModifier.kt
@@ -5,3 +5,11 @@ package com.zkp.breath.kotlin
 //protected  同一个文件中或子类可见
 //public     所有调用的地方都可见
 //internal   同一个模块中可见
+
+
+/**
+ * 1.kotlin的编译器常量是否能自定义访问器
+ * 2.请说话when操作符和java的哪个操作赋符作用对应？
+ * 3.类委托相当于什么设计模式？抽象类和接口是否都能使用代理？属性代理是否能自定义访问器
+ * 4.DelegationDemo的170行的例子，输出是哪个？
+ */
