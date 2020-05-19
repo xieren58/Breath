153532681cddb57f9534e44838c7afaa1b864103
zwp <1258520250@qq.com>
2020-05-19 16:09:48 +0800

    update

--- app/src/main/java/com/zkp/breath/component/fragment/base/BaseFragment.kt
+++ app/src/main/java/com/zkp/breath/component/fragment/base/BaseFragment.kt
@@ -15,9 +15,30 @@ import androidx.fragment.app.Fragment
 abstract class BaseFragment(@LayoutRes contentLayoutId: Int = 0) : Fragment(contentLayoutId) {
 
     val TAG = this::class.simpleName
+    /**
+     * 需要使用到Activity的时候自己强转即可
+     */
+    private lateinit var mContext: Context
 
+
+    /**
+     * <p>
+     * 解决getActivity()可能返回null，requireActivity()内部也是调用getActivity()只是会对结果进行判断，
+     * 判断为null会抛出异常，所以这个方法调用的时候需要try-catch。
+     *
+     * <p>
+     * 如果在创建Fragment时要传入参数，必须要通过setArguments(Bundle bundle)方式添加，而不建议通过为Fragment
+     * 添加带参数的构造函数，因为通过setArguments()方式添加，在由于内存紧张导致Fragment被系统杀掉并恢复
+     * （re-instantiate）时能保留这些数据，这是官方建议的做法。
+     *
+     *  <p>
+     * 如果要获取setArguments(Bundle bundle)设置的参数，我们可以在下面的onAttach()中通过getArguments()
+     * 获取。
+     */
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        this.mContext = context
+        val arguments = arguments
         Log.i(tag, "onAttach()")
     }
 
@@ -72,6 +93,20 @@ abstract class BaseFragment(@LayoutRes contentLayoutId: Int = 0) : Fragment(cont
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         Log.i(tag, "onActivityCreated()")
+    }
+
+    /**
+     * FragmentTransaction的commit()操作是异步的，内部通过mManager.enqueueAction()加入处理队列。
+     * 对应的同步方法为commitNow()，commit()内部会有checkStateLoss()操作，如果开发人员使用不当（比如commit()操
+     * 作在onSaveInstanceState()之后），可能会抛出异常，而commitAllowingStateLoss()方法则是不会抛出异常版本
+     * 的commit()方法，但是尽量使用commit()，而不要使用commitAllowingStateLoss()。
+     *
+     * addToBackStack("fname")是可选的。FragmentManager拥有回退栈（BackStack），类似于Activity的任务栈，
+     * 如果添加了该语句，就把该事务加入回退栈（压栈），当用户点击返回按钮，会回退该事务（回退指的是如果事务是add(frag1)，
+     * 那么回退操作就是remove(frag1)，即出栈）；如果没添加该语句，用户点击返回按钮会直接销毁Activity。
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
