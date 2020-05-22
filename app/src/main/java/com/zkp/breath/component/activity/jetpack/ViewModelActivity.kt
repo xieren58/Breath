30aff6c793197c8917d18371460f7e3db75a35c8
zwp <1258520250@qq.com>
2020-05-22 16:36:46 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/jetpack/ViewModelActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/jetpack/ViewModelActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.blankj.utilcode.util.ToastUtils
 import com.zkp.breath.component.activity.base.BaseActivity
 import com.zkp.breath.databinding.ActivityVmBinding
+import com.zkp.breath.jetpack.viewmodel.JetPackAndroidViewModel
 import com.zkp.breath.jetpack.viewmodel.JetPackViewModel
 
 /**
@@ -24,27 +25,33 @@ import com.zkp.breath.jetpack.viewmodel.JetPackViewModel
  *
  * 注意：
  * 1.Activity销毁的时候，框架会调用 ViewModel 对象的 onCleared() 方法，我们可以在这个方法进行释放资源的操作。
- * 2.
+ * 2.ViewModel 绝不能引用视图、Lifecycle 或可能存储对 Activity 上下文的引用的任何类。因为vm是数据持久，而视图，Lifecycle
+ *  （其实就是activity，fragment），引用activity上下文的类都会引起内存泄露。但是可以包含LifecycleObservers，如 LiveData 对象
+ * 3. 如果 ViewModel 需要 Application 上下文（例如，为了查找系统服务），它可以扩展 AndroidViewModel 类并设置用
+ *    于接收 Application 的构造函数。
  */
 class ViewModelActivity : BaseActivity() {
 
     private lateinit var binding: ActivityVmBinding
     private lateinit var viewModel: JetPackViewModel
+    private lateinit var androidViewModel: JetPackAndroidViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVmBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
-//        val jetPackViewModel = JetPackViewModel()
-//        lifecycle.addObserver(jetPackViewModel)
-//        viewModel = ViewModelProvider(this).get(jetPackViewModel.javaClass)
-
         viewModel = ViewModelProvider(this).get(JetPackViewModel::class.java)
+
+        androidViewModel = ViewModelProvider(this).get(JetPackAndroidViewModel::class.java)
 
         // 主线程调用observe()方法
         viewModel.initData()?.observe(this, Observer<String> {
             ToastUtils.showShort("数据发生改变:$it")
+        })
+
+        androidViewModel.initData()?.observe(this, Observer<String> {
+            ToastUtils.showShort("AndroidViewModel数据发生改变:$it")
         })
 
     }
