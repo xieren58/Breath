dc6c95a20a795b59ef824135e3cca901725b1d7b
zwp <1258520250@qq.com>
2020-05-22 14:09:15 +0800

    update

--- app/src/main/java/com/zkp/breath/jetpack/livedata/JetPackLiveData.kt
+++ app/src/main/java/com/zkp/breath/jetpack/livedata/JetPackLiveData.kt
@@ -1,9 +1,6 @@
 package com.zkp.breath.jetpack.livedata
 
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.blankj.utilcode.util.ToastUtils
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
@@ -14,8 +11,31 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 class JetPackLiveData : ViewModel(), DefaultLifecycleObserver {
 
     var data: MutableLiveData<String>? = null
-    val mTasks: ListCompositeDisposable = ListCompositeDisposable()
+    private val mTasks: ListCompositeDisposable = ListCompositeDisposable()
     val cusLiveData = CusLiveData()
+
+    // MediatorLiveData的使用例子
+    var mediatorLiveData: MediatorLiveData<String> = MediatorLiveData()
+    private var liveData1 = MutableLiveData<String>()
+    private var liveData2 = MutableLiveData<String>()
+    private var liveData3 = MutableLiveData<String>()
+
+
+    init {
+        // 即时观察者，也是被观察者。
+        // 合并多个 LiveData 源，只要任何原始的 LiveData 源对象发生更改，就会触发 MediatorLiveData 对象的观察者
+        // mediatorLiveData监听其它的liveData源，然后可以通过调用自己的setValue（）告诉观察者。
+        mediatorLiveData.addSource(liveData1) {
+            mediatorLiveData.value = it
+        }
+        mediatorLiveData.addSource(liveData2) {
+            mediatorLiveData.value = it
+        }
+        mediatorLiveData.addSource(liveData3) {
+            mediatorLiveData.value = it
+        }
+    }
+
 
     fun initData(): MutableLiveData<String>? {
         if (data == null) {
@@ -38,6 +58,10 @@ class JetPackLiveData : ViewModel(), DefaultLifecycleObserver {
 
                         override fun onNext(t: String?) {
                             data?.value = t
+
+                            liveData1.value = "1"
+//                            liveData2.value = "2"
+//                            liveData3.value = "3"
                         }
 
                         override fun onError(e: Throwable?) {
