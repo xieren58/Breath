a3377940762155c82a9a655f9a506f9366cebd45
zwp <1258520250@qq.com>
2020-05-25 15:19:18 +0800

    添加room的demo

---
+++ app/src/main/java/com/zkp/breath/component/activity/jetpack/RoomActivity.kt
@@ -0,0 +1,19 @@
+package com.zkp.breath.component.activity.jetpack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.zkp.breath.component.activity.base.BaseActivity
+import com.zkp.breath.databinding.ActivityRoomBinding
+import com.zkp.breath.databinding.ActivityVmBinding
+
+
+class RoomActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityRoomBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRoomBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+    }
+}
