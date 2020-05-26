6e30c0179a663eeaa3321a0d6f5ec55bba374a8a
zwp <1258520250@qq.com>
2020-05-26 09:47:41 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
@@ -5,7 +5,7 @@ package com.zkp.breath.kotlin
  * 数据类，对应java的Bean类，组建函数componentN()，解构声明，copy函数。
  * 解构声明：内部调用的是组建函数componentN()，但是能解析的只用定义在类名后的主构函数的成员属性。
  *
- * 注意：数组也能使用解构声明。
+ * 注意：数组和集合也能使用解构声明。
  */
 data class DataClass(val s: String, var i: Int) {
     val ss: String = ""
@@ -34,7 +34,12 @@ fun main() {
 
     // 数组也能使用解构声明
     val (i1, i2, i3) = arrayOf(1, 2, 3)
-    println("i1: ${i1}, i2: ${i2}, i3: ${i3}")
+    println("i1: ${i1}, i2: ${i2}, i3: $i3")
+
+    // 集合也能使用解构声明
+    val (a1, a2, a3) = arrayListOf(1, 2, 3)
+    println("i1: ${a1}, i2: ${a2}, i3: $a3")
+
 
     // Map优雅的遍历方式
     // 接收参数接收的是一个Pair，Pair本身也是Data类
