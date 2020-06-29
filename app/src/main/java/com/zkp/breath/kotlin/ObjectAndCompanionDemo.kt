cad35c886d9ba296a1cc845fb264b600f4e3e26f
zwp <1258520250@qq.com>
2020-06-29 11:41:38 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/ObjectAndCompanionDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/ObjectAndCompanionDemo.kt
@@ -1,19 +1,19 @@
 package com.zkp.breath.kotlin
 
 /**
- * 1.object类其实就是饿汉式线程安全的单例，object修饰的类也可以实现接口；object：也可以用来创建匿名类的对象。
+ * 1.object类其实就是饿汉式线程安全的单例，object修饰的类也可以实现接口；object也可以用来创建匿名类的对象。
  * 2.companion object伴随外部类而存在，一个类中最多只有一个伴生对象。Java 静态变量和方法的等价写法：companion object 中的变量和函数。
  *   其实companion object修饰的类在外部类被加载的时候就随之被实例化，所以实际上还是通过实例去调用方法/变量，虽然说是等价
  *   java的静态变量和静态方法的写法，但实际其实不是，且kotlin没有静态变量和静态方法这两个概念。
  *
  *
- * 静态方法或者静态变量在实际使用中，在 object、companion object 和 top-level 中应如何选择：
+ * 在实际使用中，在 object、companion object 和 top-level 中应如何选择：
  * 1.如果想写工具类的功能，直接创建文件，写 top-level「顶层」函数。推荐
  * 2.如果需要继承别的类或者实现接口，用companion object或者object，但是object是一个单例还是不要泛滥使用，
  *  而companion object的使用是与外部类存在某种关联才去使用。
  *
- * 常量：
- * Kotlin 的常量（const val）必须声明在对象（包括伴生对象）或者「top-level 顶层」中，
+ * 常量（编译时常量）：
+ * Kotlin 的常量（const val）必须声明在对象（包括伴生对象）或者「top-level 顶层」中，因为常量是静态的。
  * Kotlin 中只有基本类型和 String 类型可以声明成常量（防止其他类型实例后对内部的变量进行修改）。
  */
 
@@ -116,7 +116,11 @@ interface TempI
 
 class Four {
     companion object {
+        // 静态变量
         var s: String = "伴生对象的变量"
+        const val CONST_NUMBER = 1
+        val s2: String = "ss"
+
         fun function() {
             println("伴生对象的方法")
         }
