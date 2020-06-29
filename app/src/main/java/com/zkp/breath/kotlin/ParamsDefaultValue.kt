a70e0a6c40b62a31f3c6c35586fcc1997a684240
zwp <1258520250@qq.com>
2020-06-29 17:51:38 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/ParamsDefaultValue.kt
+++ app/src/main/java/com/zkp/breath/kotlin/ParamsDefaultValue.kt
@@ -1,7 +1,8 @@
 package com.zkp.breath.kotlin
 
 /**
- * 参数默认值
+ * 参数默认值：参数存在默认值
+ * 命名参数：调用方法的时候使用key=value的方式传入参数
  */
 
 fun sayHi(name: String = "world") = println("Hi $name")
@@ -13,7 +14,7 @@ fun main() {
     // 调用一个参数方法
     sayHi("")
 
-    // 调用两个参数的方法
+    // 调用两个参数的方法存在默认值的参数，调用的时候需要使用命名参数的方式
     // sayHi(10)  // 这种写法是不被允许的
     // sayHi(name = "", 10) // 这种写法是不被允许的
     // sayHi(age = 10, "")  // 这种写法是不被允许的
