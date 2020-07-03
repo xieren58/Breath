0312838bd670b4beb0d5ff713ca1e3199e4a7ad0
zwp <1258520250@qq.com>
2020-07-03 16:17:34 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
@@ -20,17 +20,17 @@ fun main() {
     val dataClass = DataClass("数据类", 22)
     println("toString(): ${dataClass.toString()}")
     println("componentN()组件函数: ${dataClass.component1()}")
-    val copy = dataClass.copy("数据类copy", 33)
+    val copy = dataClass.copy(s = "数据类copy", i = 33)
     val copy1 = dataClass.copy()
-    println("copy类: ${copy.toString()}")
+    println("copy类: $copy")
     println("copy内存地址比较: ${dataClass === copy1}")
     // 解构声明，其实内部调用的就是组建函数
     val (s, i) = copy1
     // 直接调用数据类对象其实内部调用是toString()方法
     println("copy1的toString(): ${copy1}")
     // 标准库提供的数据类
-    val pair = Pair<String, String>("a", "b")
-    val triple = Triple<String, String, String>("a", "b", "c")
+    val pair = Pair("a", "b")
+    val triple = Triple("a", "b", "c")
     // 下划线表示没有使用到的变量
     val (_, s2) = pair
     println("打印参数2：${s2}")
