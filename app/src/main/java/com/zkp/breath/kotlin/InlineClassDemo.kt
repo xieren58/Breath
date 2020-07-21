c3339aa48c24db5bee50c3f768e61c72af39c34e
zwp <1258520250@qq.com>
2020-07-21 14:53:35 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/InlineClassDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/InlineClassDemo.kt
@@ -14,8 +14,9 @@ package com.zkp.breath.kotlin
  *
  *  注意：现在内联类还是一个实验功能
  *
- *  1.一定要公有主构造函数，且构造函数中声明只一个val修饰的基本数据类型
- *  2.没有init初始化块
+ *
+ *  1.内联类必须包含一个基础值，这就意味它需要一个公有主构造器来接收，主构造函数中只声明一个val修饰的数据类型。
+ *  2.没有init初始化块。
  *  3.可以声明val属性（不能是延迟或者委托）和函数
  *  4.属性没有幕后字段，没有初始器，只能使用get方法且内部的操作还是针对构造器中的常量
  *  5. 可以实现接口，不能集成类
@@ -24,25 +25,26 @@ package com.zkp.breath.kotlin
 
 inline class Hours(val value: Int) {
 
-    val length: Int
-        get() = value
-
     fun toMinutes() = value * 60
-    // 其实相当于一个静态方法
+
     // public static final int toMinutes(int $this) {
     //	return $this * 60;
     // }
 }
 
-fun wait(period: Hours) { /* ... */ }
+fun wait(period: Hours) { /* ... */
+}
 // void wait(int period) { /* ... */ }
 
 fun main() {
     val period = Hours(24)
-    //  int period = 24;  上面的初始化在编译后相当于这样，可以看到并没有正在创建实例（不会分配到堆内存），其实就是对基本类型的封装，
+    // 上面的初始化在编译后相当于这样，可以看到并没有正在创建实例（不会分配到堆内存），它只是将基础值分配给int类型的变量。
+    //  int period = 24;
+
+    // 如果我们在Kotlin中调用Hours(24).toMinutes()，它可以有效地编译为toMinutes(24),其实就是一个工具静态方法。
+    val toMinutes = period.toMinutes()
 
     wait(period)
+    // 上面的方法编译后如下，它只是将基础值分配给int类型的变量
     // wait(24)
-
-
 }
