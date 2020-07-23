cf54d4bbde9de09f16c535631f4d8e892d67f376
zwp <1258520250@qq.com>
2020-07-23 19:25:03 +0800

    update

--- app/src/main/java/com/zkp/breath/MainActivity.kt
+++ app/src/main/java/com/zkp/breath/MainActivity.kt
@@ -2,16 +2,22 @@ package com.zkp.breath
 
 import android.os.Bundle
 import android.view.KeyEvent
-import com.blankj.utilcode.util.ActivityUtils
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import com.blankj.utilcode.util.ToastUtils
 import com.umeng.analytics.MobclickAgent
+import com.zkp.breath.adpter.EntranceAdapter
 import com.zkp.breath.component.activity.base.BaseActivity
-import com.zkp.breath.component.activity.jetpack.ViewModelActivity
 import com.zkp.breath.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val listOf = mutableListOf(
+            "kotlin", "java", "jetpack"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,21 +25,26 @@ class MainActivity : BaseActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        umEvent()
+        initRcv()
+    }
+
+    private fun initRcv() {
+        val rcv = binding.rcv
+        rcv.itemAnimator?.changeDuration = 0
+        rcv.overScrollMode = View.OVER_SCROLL_NEVER
+        val gridLayoutManager = GridLayoutManager(rcv.context, 2)
+        rcv.layoutManager = gridLayoutManager
+        val gridAdapter = EntranceAdapter(listOf)
+        rcv.adapter = gridAdapter
+    }
+
+    private fun umEvent() {
         // 模拟UM的事件统计
         val umEventMap: MutableMap<String, Any> = HashMap()
         umEventMap["name"] = "zkp"
         umEventMap["page_name"] = "main_activity"
         MobclickAgent.onEventObject(this, "um_main_event", umEventMap)
-
-        ActivityUtils.startActivity(ViewModelActivity::class.java)
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 
     //  保存点击的时间
@@ -45,7 +56,7 @@ class MainActivity : BaseActivity() {
                 ToastUtils.showShort("再按一次退出程序")
                 exitTime = System.currentTimeMillis()
             } else {
-                System.exit(0)
+                exitProcess(0)
             }
             return false
         }
