03d3b0d38c2d39e810a2170bba020e2eec06401c
zwp <1258520250@qq.com>
2020-08-15 11:38:37 +0800

    自定义的bindingadpter必须是静态方法

--- app/src/main/java/com/zkp/breath/jetpack/databinding/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/zkp/breath/jetpack/databinding/ImageViewBindingAdapter.kt
@@ -15,12 +15,10 @@ import com.zkp.breath.R
  * 2.DataBinding的@BindingAdapter也支持同时指定多个属性（value是个可变参数，即数组）。默认来说，当指定多个属性时，
  * 这些属性要同时地使用到同一个控件上，有点像控件中强制要求设定属性layout_width和layout_height一样，否则的话会报错。
  * 我们也可以通过设置requireAll的值来指定是否强制所有的属性同时使用。
+ * 3.方法一定要是静态的。
  */
-class ImageViewBindingAdapter {
 
-    @BindingAdapter("url", requireAll = false)
-    fun bindImgUrl(imageView: ImageView, url: String?) {
-        Glide.with(imageView.context).load(url).error(R.drawable.bg_rcv_item).into(imageView)
-    }
-
+@BindingAdapter("url", requireAll = false)
+fun bindImgUrl(imageView: ImageView, url: String?) {
+    Glide.with(imageView.context).load(url).error(R.drawable.bg_rcv_item).into(imageView)
 }
