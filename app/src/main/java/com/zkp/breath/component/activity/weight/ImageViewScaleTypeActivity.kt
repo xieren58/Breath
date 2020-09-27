81d8fc4ce6efed134ae3a8b47025e33d82be2d7e
zwp <1258520250@qq.com>
2020-09-27 18:43:12 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/weight/ImageViewScaleTypeActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/weight/ImageViewScaleTypeActivity.kt
@@ -30,6 +30,7 @@ class ImageViewScaleTypeActivity : BaseActivity() {
             when (v) {
                 binding.tvScale -> {
                     xx()
+//                    yyy()
 
 
 //                    //通过系统的文件浏览器选择一个文件
@@ -65,7 +66,21 @@ class ImageViewScaleTypeActivity : BaseActivity() {
 
         val musicResolver: ContentResolver = contentResolver
         val musicUri: Uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        val musicCursor: Cursor? = musicResolver.query(Uri.parse("content://media/external_primary/file/73"), null,
+        //Android Q 公有目录只能通过Content Uri + id的方式访问，以前的File路径全部无效，如果是Video，
+        // 记得换成MediaStore.Videos
+//        var path = ""
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            path = MediaStore.Audio.Media
+//                    .EXTERNAL_CONTENT_URI
+//                    .buildUpon()
+//                    .appendPath(thisalbumId.toString())
+//                    .build()
+//                    .toString()
+//        }
+
+
+//        val musicUri: Uri = Uri.parse("content://media/external_primary/file/73")
+        val musicCursor: Cursor? = musicResolver.query(musicUri, null,
                 null, null, null)
 
         if (musicCursor != null && musicCursor.moveToFirst()) {
@@ -76,41 +91,100 @@ class ImageViewScaleTypeActivity : BaseActivity() {
             val albumId = musicCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)
             val data = musicCursor.getColumnIndex(MediaStore.Audio.Media.DATA)
             val albumkey = musicCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_KEY)
-
-
             val columnIndex = musicCursor.getColumnIndex(MediaStore.Audio.Media.YEAR)
             val columnIndex1 = musicCursor.getColumnIndex(MediaStore.Audio.Media.DURATION)
-
 
             do {
                 val thisId = musicCursor.getLong(idColumn)
                 val thisTitle = musicCursor.getString(titleColumn)
-                val thisArtist = musicCursor.getString(artistColumn)
-                val thisalbumId = musicCursor.getLong(albumId)
-                val thisdata = musicCursor.getString(data)
-                val AlbumKey = musicCursor.getString(albumkey)
-                val AlbumKey1 = musicCursor.getString(columnIndex)
-                val AlbumKey2 = musicCursor.getString(columnIndex1)
+                val thisArtist = try {
+                    musicCursor.getString(artistColumn)
+                } catch (e: Exception) {
+                    ""
+                }
+                val thisalbumId = try {
+                    musicCursor.getLong(albumId)
+                } catch (e: Exception) {
+                    0L
+                }
+                val thisdata = try {
+                    musicCursor.getString(data)
+                } catch (e: Exception) {
+                    ""
+                }
+                val AlbumKey = try {
+                    musicCursor.getString(albumkey)
+                } catch (e: Exception) {
+                }
+                val AlbumKey1 = try {
+                    musicCursor.getString(columnIndex)
+                } catch (e: Exception) {
+                    ""
+                }
+                val AlbumKey2 = try {
+                    musicCursor.getString(columnIndex1)
+                } catch (e: Exception) {
+                    ""
+                }
 
 
-                //Android Q 公有目录只能通过Content Uri + id的方式访问，以前的File路径全部无效，如果是Video，
-                // 记得换成MediaStore.Videos
-//                var path = ""
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                    path = MediaStore.Audio.Media
-//                            .EXTERNAL_CONTENT_URI
-//                            .buildUpon()
-//                            .appendPath(thisalbumId.toString())
-//                            .build()
-//                            .toString()
-//                }
-
-                Log.i("xxxx", "thisId:$thisId,thisTitle:$thisTitle,thisArtist:$thisArtist,thisalbumId:$thisalbumId,thisdata:$thisdata,AlbumKey:$AlbumKey")
-//                Log.i("xxxx", "thisId:$thisId,thisTitle:$thisTitle,thisArtist:$thisArtist,thisalbumId:$thisalbumId,path:$data")
-
+                Log.i("xxxx", "thisId:$thisId,thisTitle:$thisTitle,thisArtist:$thisArtist," +
+                        "thisalbumId:$thisalbumId,thisdata:$thisdata,AlbumKey:$AlbumKey")
             } while (musicCursor.moveToNext())
 
         }
+    }
+
+    private val MIMETable = arrayOf<kotlin.Array<kotlin.String>>(
+            arrayOf<kotlin.String>("mp3", "audio/x-mpeg"))
+
+    private fun yyy() {
+
+
+        var t = ""
+        val type = "mp3" //要查找的文件后缀
+
+        val resolver = contentResolver
+        val uri = MediaStore.Files.getContentUri("external")
+        for (i in MIMETable.indices) {
+            if (type == MIMETable[i][0]) {
+                t = MIMETable.get(i).get(1)
+            }
+        }
+
+        val cursor = resolver.query(uri, /*arrayOf(MediaStore.Files.FileColumns.DATA,
+                MediaStore.Files.FileColumns.DATE_ADDED, MediaStore.Files.FileColumns.SIZE,
+                MediaStore.Files.FileColumns.TITLE)*/null,
+                MediaStore.Files.FileColumns.MIME_TYPE + " = '" + t + "'",
+                null, null)
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val title = try {
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.TITLE))
+                } catch (e: Exception) {
+                    ""
+                }
+                val path = try {
+                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA))
+                } catch (e: Exception) {
+                    ""
+                }
+                val time = try {
+                    cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATE_ADDED))
+                } catch (e: Exception) {
+                    -1
+                }
+                val size = try {
+                    cursor.getInt(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.SIZE))
+                } catch (e: Exception) {
+                    -1
+                }
+                Log.i("xxxx", "title:$title,path:$path,time:$time," +
+                        "size:$size")
+            }
+        }
+        cursor!!.close()
+
     }
 
 
