909149d42787cc862a932a5e7ee2190fd3b47958
zkp <1258520250@qq.com>
2020-10-13 23:07:47 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/debugs/DebugActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/debugs/DebugActivity.kt
@@ -16,6 +16,10 @@ import kotlin.concurrent.thread
 
 /**
  * https://mp.weixin.qq.com/s/ppJ-pRDifyPpQoPIyYbxww
+ *
+ * 1. 禁用断点 (并非删除断点)，可以右键点击断点并从弹框中取消选中 Enabled 选框。您也可以通过按住 Alt (在 Mac 上
+ * 是 Option) 并点击断点，从而更快速地禁用断点。
+ * 2. 断点列表对断点分组
  */
 class DebugActivity : BaseActivity() {
 
@@ -62,6 +66,9 @@ class DebugActivity : BaseActivity() {
                 // 日志断点：取消选中Suspend，选Evaluate and log选项，并在其中添加上Log语句，点击‘Done’按钮。
                 // 和调用Log日志工具类起到一样的作用，只是可以避免在类中到处打Log，影响类的可读性，日志断点严格来说并不是断点调试，
                 // 它不会在你打断点的地方停下来，它只是让你在需要的地方输出日志而已。
+
+                // 如果只是想快速验证断点是否触发并且不在乎其中的细节信息，可以使用 "Breakpoint hit" 信息
+                // 来记录断点的触发事件，在Debug的Console中能看到打印信息，信息指向断点位置。
                 super.onScrollStateChanged(recyclerView, newState)
             }
         })
