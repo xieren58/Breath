3b402bcec8475b86c8de1866e0f159f2fe95c988
zwp <1258520250@qq.com>
2020-10-14 18:30:53 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/weight/SwitchActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/weight/SwitchActivity.kt
@@ -6,7 +6,17 @@ import android.view.LayoutInflater
 import com.zkp.breath.component.activity.base.BaseActivity
 import com.zkp.breath.databinding.ActivitySwitchBinding
 
-
+/**
+ * https://blog.csdn.net/jiangtea/article/details/71189438
+ * https://www.jianshu.com/p/796e2f7f0ed2
+ *
+ *
+ * 原生的switch的缺点：
+ * 1.layout_width和layout_height只能wrap_content，如果设置match_parent或者具体数据其实相当于设置
+ *   父控件的大小（这时候可以看成还有一层父控件）
+ * 2.只能通过switchMinWidth设置宽度
+ * 3.
+ */
 class SwitchActivity : BaseActivity() {
 
     private lateinit var binding: ActivitySwitchBinding
@@ -16,17 +26,13 @@ class SwitchActivity : BaseActivity() {
         binding = ActivitySwitchBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
 
-//        thumbTint：按钮（圆）颜色
-//        trackTint：轨道（椭圆）颜色
-
         binding.switchCompat.setOnCheckedChangeListener { compoundButton, b ->
-            if (b) {
-                binding.switchCompat.thumbTintList = ColorStateList.valueOf(0xffFAFAFA.toInt())
-                binding.switchCompat.trackTintList = ColorStateList.valueOf(0xffE8E8E8.toInt())
-            } else {
-                binding.switchCompat.thumbTintList = ColorStateList.valueOf(0xff8844FF.toInt())
-                binding.switchCompat.trackTintList = ColorStateList.valueOf(0x808844FF.toInt())
-            }
+
+            binding.switchCompat.thumbTintList = ColorStateList.valueOf(
+                    if (b) 0xff8844FF.toInt() else 0xffFAFAFA.toInt())
+
+            binding.switchCompat.trackTintList = ColorStateList.valueOf(
+                    if (b) 0x808844FF.toInt() else 0xffE8E8E8.toInt())
         }
     }
 
