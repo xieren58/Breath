31828ca710c11d560766fed19534b517c09ab6ef
zwp <1258520250@qq.com>
2020-10-20 19:51:48 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/weight/ButtonActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/weight/ButtonActivity.kt
@@ -7,7 +7,9 @@ import com.blankj.utilcode.util.ClickUtils
 import com.zkp.breath.component.activity.base.BaseActivity
 import com.zkp.breath.databinding.ActivityButtonBinding
 
-
+/**
+ * selector中的state_pressed="true"对于设置监听点击事件的TextView控件才有效，否则无效
+ */
 class ButtonActivity : BaseActivity() {
 
     private lateinit var binding: ActivityButtonBinding
@@ -17,12 +19,15 @@ class ButtonActivity : BaseActivity() {
         binding = ActivityButtonBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
         binding.tv.setOnClickListener(onClick)
+        // tv开启点击事件，这样selector中的state_pressed="true"才能生效
+        binding.tvBtn.setOnClickListener(onClick)
     }
 
     val onClick = object : ClickUtils.OnDebouncingClickListener() {
         override fun onDebouncingClick(v: View?) {
             if (v == binding.tv) {
                 binding.button.isEnabled = !binding.button.isEnabled
+                binding.tvBtn.isEnabled = !binding.tvBtn.isEnabled
             }
         }
     }
