7bf542be02bc08aa6499dc5492508d88b6ec3883
zwp <1258520250@qq.com>
2020-10-21 14:47:00 +0800

    update

--- app/src/main/java/com/zkp/breath/MainActivity.kt
+++ app/src/main/java/com/zkp/breath/MainActivity.kt
@@ -54,61 +54,83 @@ class MainActivity : BaseActivity() {
         rcv.adapter = gridAdapter
     }
 
+
+    /**
+     * 非内联系函数的lambda表达式不允许直接return，只能使用"@+标签"退出lambda表达式
+     */
     private val onItemChildClickListener = OnItemClickListener { _, _, position ->
         when (val type = listOf[position]) {
             "Button" -> {
                 ActivityUtils.startActivity(ButtonActivity::class.java)
+                return@OnItemClickListener
             }
             "Switch" -> {
                 ActivityUtils.startActivity(SwitchActivity::class.java)
+                return@OnItemClickListener
             }
             "ImageViewScaleType" -> {
                 ActivityUtils.startActivity(ImageViewScaleTypeActivity::class.java)
+                return@OnItemClickListener
             }
             "MotionLayout" -> {
                 ActivityUtils.startActivity(MotionLayoutActivity::class.java)
+                return@OnItemClickListener
             }
             "Debug" -> {
                 ActivityUtils.startActivity(DebugActivity::class.java)
+                return@OnItemClickListener
             }
             "Rxjava3" -> {
                 ActivityUtils.startActivity(RxJava3Activity::class.java)
+                return@OnItemClickListener
             }
             "Glide4" -> {
                 ActivityUtils.startActivity(GlideActivity::class.java)
+                return@OnItemClickListener
             }
             "Fragment" -> {
                 ActivityUtils.startActivity(FragmentDemoActivity::class.java)
+                return@OnItemClickListener
             }
             "ViewEvent" -> {
                 ActivityUtils.startActivity(EventActivity::class.java)
+                return@OnItemClickListener
             }
             "Service" -> {
                 ActivityUtils.startActivity(ServiceDemoActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackStartUp" -> {
                 ActivityUtils.startActivity(StartupActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackViewModel" -> {
                 ActivityUtils.startActivity(ViewModelActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackViewBinding" -> {
                 ActivityUtils.startActivity(ViewBindingActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackLifecycle" -> {
                 ActivityUtils.startActivity(LifecycleActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackLiveData" -> {
                 ActivityUtils.startActivity(LiveDataActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackDataBinding" -> {
                 ActivityUtils.startActivity(DataBindingActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackPaging" -> {
                 ActivityUtils.startActivity(PagingActivity::class.java)
+                return@OnItemClickListener
             }
             "JetPackRoom" -> {
                 ActivityUtils.startActivity(RoomActivity::class.java)
+                return@OnItemClickListener
             }
             else -> {
                 ToastUtils.showShort(type)
