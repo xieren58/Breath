f5b2df601d98a1d66da0a7826478be032fe69a7f
zkp <1258520250@qq.com>
2020-11-02 21:27:07 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/weight/ButtonActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/weight/ButtonActivity.kt
@@ -1,6 +1,5 @@
 package com.zkp.breath.component.activity.weight
 
-import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
@@ -10,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.LinearInterpolator
 import com.blankj.utilcode.util.ClickUtils
-import com.blankj.utilcode.util.CollectionUtils
 import com.blankj.utilcode.util.ScreenUtils
 import com.blankj.utilcode.util.SizeUtils
 import com.zkp.breath.component.activity.base.BaseActivity
@@ -23,8 +21,11 @@ import com.zkp.zkplib.anim.ObjectAnimatorAssist
 class ButtonActivity : BaseActivity() {
 
     private lateinit var binding: ActivityButtonBinding
-
-    val broadcastEventList: MutableList<String> = mutableListOf()
+    private val animatorSet1 = AnimatorSet()
+    private val animatorSet2 = AnimatorSet()
+    private val animatorSet3 = AnimatorSet()
+    private val animatorSet4 = AnimatorSet()
+    private val speed = 0.17f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,37 +36,12 @@ class ButtonActivity : BaseActivity() {
         binding.tvBtn.setOnClickListener(onClick)
     }
 
-
     val onClick = object : ClickUtils.OnDebouncingClickListener() {
         override fun onDebouncingClick(v: View?) {
             if (v == binding.tv) {
-//                binding.button.isEnabled = !binding.button.isEnabled
-//                binding.tvBtn.isEnabled = !binding.tvBtn.isEnabled
-
-//                animator()
-//                animator2()
-                xxx()
-            }
-        }
-    }
-
-    private fun pushBroadcastEvent(event: String) {
-        if (CollectionUtils.isEmpty(broadcastEventList)) {
-            broadcastEventList.add(event)
-            // todo 根据数据判断类型，启动礼物广播或者送礼广播
-        } else {
-            broadcastEventList.add(event)
-        }
-    }
-
-    private fun popBroadcastEvent() {
-        if (!CollectionUtils.isEmpty(broadcastEventList)) {
-            try {
-                broadcastEventList.removeAt(0)
-            } catch (e: Exception) {
-            }
-            if (!CollectionUtils.isEmpty(broadcastEventList)) {
-                // todo 根据数据判断类型，启动礼物广播或者送礼广播
+                binding.button.isEnabled = !binding.button.isEnabled
+                binding.tvBtn.isEnabled = !binding.tvBtn.isEnabled
+                rewardBannerAnimator()
             }
         }
     }
@@ -87,7 +63,7 @@ class ButtonActivity : BaseActivity() {
         animator1.duration = 528L
         animator1.interpolator = LinearInterpolator()
 
-        val contentShowTime = (scrollX / 0.17f).toLong()    // 时间
+        val contentShowTime = (scrollX / speed).toLong()
 
         val animator2: ObjectAnimator = ObjectAnimator.ofFloat(
                 binding.tvGiftContent,
@@ -107,11 +83,8 @@ class ButtonActivity : BaseActivity() {
         animator3.startDelay = 528L + 1980L + 1650L + contentShowTime
         animator3.interpolator = LinearInterpolator()
 
-        // todo 声明为成员变量，及时回收
-        val animatorSet = AnimatorSet()
-        animatorSet.playTogether(animator1, animator2, animator3)
-        animatorSet.addListener(animatorListener)
-        animatorSet.start()
+        animatorSet1.playTogether(animator1, animator2, animator3)
+        animatorSet1.start()
     }
 
     fun staticContentGiftAnimator() {
@@ -135,60 +108,56 @@ class ButtonActivity : BaseActivity() {
         animator2.startDelay = 528L + 2310L
         animator2.interpolator = LinearInterpolator()
 
-        // todo 声明为成员变量，及时回收
-        val animatorSet = AnimatorSet()
-        animatorSet.playTogether(animator1, animator2)
-        animatorSet.addListener(animatorListener)
-        animatorSet.start()
+
+        animatorSet2.playTogether(animator1, animator2)
+        animatorSet2.start()
     }
 
-    fun xxx() {
+    fun rewardBannerAnimator() {
         // 送礼
-        binding.tvGiftRich.setText("卡卡罗特 给 ")
-        binding.tvGiftContent.setText("咖啡不加糖呀、天青色等烟雨，sdasda,dasdsdas,2312321,423335,5454343火箭x1")
+        binding.tvGiftRich.text = "卡卡罗特 给 "
+        binding.tvGiftContent.text = "咖啡不加糖呀、天青色等烟雨、相机、小丑、面具、哈哈、呵呵、嘿嘿、嘻嘻火箭x1"
 
         // 文字长度是否超过边框宽度，是则进行滚动动画，否则进行不滚动动画
         SizeUtils.forceGetViewSize(binding.hsvGift, object : SizeUtils.OnGetSizeListener {
             override fun onGetSize(view: View?) {
-                val width = view?.width
-                val measuredWidth2 = SizeUtils.getMeasuredWidth(binding.tvGiftContent)
+                val scrollViewWidth = view?.width
+                val tvContentWidth = SizeUtils.getMeasuredWidth(binding.tvGiftContent)
 
-                if (width == null) {
+                if (scrollViewWidth == null) {
                     staticContentGiftAnimator()
                     return
                 }
 
-                if (measuredWidth2 > width) {
-                    dynamicContentGiftAnimator(measuredWidth2 - width)
+                if (tvContentWidth > scrollViewWidth) {
+                    dynamicContentGiftAnimator(tvContentWidth - scrollViewWidth)
                 } else {
                     staticContentGiftAnimator()
                 }
 
-
-                Log.i("获取测试宽", "measuredWidth1: ${width}, measuredWidth2:$measuredWidth2")
+                Log.i("获取测试宽", "scrollViewWidth1: ${scrollViewWidth}, tvContentWidth1:$tvContentWidth")
             }
         })
 
-
         // 抽奖
-        binding.tvLotteryContent.setText("咖啡不加糖呀、天青色等烟雨，sdasda,dasdsdas,2312321,423335,5454343火箭x1")
+        binding.tvLotteryContent.text = "咖啡不加糖呀、天青色等烟雨、相机、小丑、面具、哈哈、呵呵、嘿嘿、嘻嘻火箭x1"
 
         SizeUtils.forceGetViewSize(binding.hsvLottery, object : SizeUtils.OnGetSizeListener {
             override fun onGetSize(view: View?) {
-                val width = view?.width
-                val measuredWidth2 = SizeUtils.getMeasuredWidth(binding.tvLotteryContent)
+                val scrollViewWidth = view?.width
+                val tvContentWidth = SizeUtils.getMeasuredWidth(binding.tvLotteryContent)
 
-                if (width == null) {
+                if (scrollViewWidth == null) {
                     staticContentLotteryAnimator()
                     return
                 }
 
-                if (measuredWidth2 > width) {
-                    dynamicContentLotteryAnimator(measuredWidth2 - width)
+                if (tvContentWidth > scrollViewWidth) {
+                    dynamicContentLotteryAnimator(tvContentWidth - scrollViewWidth)
                 } else {
                     staticContentLotteryAnimator()
                 }
-                Log.i("获取测试宽", "measuredWidth1: ${width}, measuredWidth2:$measuredWidth2")
+                Log.i("获取测试宽", "scrollViewWidth2: ${scrollViewWidth}, tvContentWidth2:$tvContentWidth")
             }
         })
     }
@@ -214,13 +183,9 @@ class ButtonActivity : BaseActivity() {
         animator2.startDelay = 528L + 1650L
         animator2.interpolator = LinearInterpolator()
 
-        // todo 声明为成员变量，及时回收
-        val animatorSet = AnimatorSet()
-        animatorSet.playTogether(animator1, animator2)
-        animatorSet.addListener(animatorListener)
-        animatorSet.start()
+        animatorSet3.playTogether(animator1, animator2)
+        animatorSet3.start()
     }
-
 
     @SuppressLint("ClickableViewAccessibility")
     fun dynamicContentLotteryAnimator(scrollX: Int) {
@@ -238,7 +203,7 @@ class ButtonActivity : BaseActivity() {
         animator1.duration = 528L
         animator1.interpolator = LinearInterpolator()
 
-        val contentShowTime = (scrollX / 0.17f).toLong()
+        val contentShowTime = (scrollX / speed).toLong()
         val animator2: ObjectAnimator = ObjectAnimator.ofFloat(
                 binding.tvLotteryContent,
                 ObjectAnimatorAssist.ObjectAnimatorType.TRANSLATION_X,
@@ -257,21 +222,16 @@ class ButtonActivity : BaseActivity() {
         animator3.startDelay = 528L + 1650L + 1650L + contentShowTime
         animator3.interpolator = LinearInterpolator()
 
-        val animatorSet = AnimatorSet()
-        animatorSet.playTogether(animator1, animator2, animator3)
-        animatorSet.addListener(animatorListener)
-        animatorSet.start()
+        animatorSet4.playTogether(animator1, animator2, animator3)
+        animatorSet4.start()
     }
 
-
-    val animatorListener = object : ObjectAnimatorAssist.SimpleAnimatorListener() {
-
-        override fun onAnimationEnd(animation: Animator?) {
-            super.onAnimationEnd(animation)
-            // todo 准备好拿下一条数据
-            popBroadcastEvent()
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        animatorSet1.cancel()
+        animatorSet2.cancel()
+        animatorSet3.cancel()
+        animatorSet4.cancel()
     }
-
 
 }
