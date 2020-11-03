5c7abfcf5958bd996189d68ca271ec0755a646d7
zkp <1258520250@qq.com>
2020-11-03 22:32:43 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/weight/ImageViewScaleTypeActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/weight/ImageViewScaleTypeActivity.kt
@@ -19,8 +19,8 @@ import com.zkp.breath.databinding.ActivityIvScaleTypeBinding
  *
  * ImageView的ScaleType:
  * FIT_XY：不按图片原比例伸缩，强制让图片充满ImageVie，图片可以完整显示但可能会变形。
- * FIT_CENTER，FIT_END，FIT_START：按照图片原比例伸缩，直到图片最长边和ImageView对应的宽或高重叠，
- *                      能够保证图片完整显示。因为要"保证图片能够完整显示"，所以只要操作最长边就能实现。
+ * FIT_CENTER，FIT_END，FIT_START：按照图片原比例伸缩，能够保证图片完整显示（基本上图片的一边会和控件对应的一边重叠）
+ *                                  如果dHeight> dWidth，则使用vWidth/dWidth作为缩放比例，反之使用vHeight/dHeight作为缩放比例。
  * CENTER：不进行任何伸缩，图片中点和ImageView重叠，按照ImageView的宽高裁剪图片，保证图片居中显示，不保证图片能够完整显示或者填满控件。
  * CENTER_CROP：按图片比例以可能裁切掉部分图片为代价，让图片充满ImageView。因为要"保证图片充满控件"，所以只要操作最短边就能实现。
  * CENTER_INSIDE：当原图任意一边长度大于ImageView的对应边时，相当于FIT_CENTER。当原图两边长度都小于等于ImageView的时候，相当于CENTER。
@@ -218,14 +218,9 @@ class ImageViewScaleTypeActivity : BaseActivity() {
             SCALE_TYPE_FIT_XY -> {
                 str = "不按图片原比例伸缩，强制让图片充满ImageVie，图片可以完整显示但可能会变形。"
             }
-            SCALE_TYPE_FIT_START -> {
-                str = "按照图片原比例伸缩，直到图片最长边和ImageView对应的宽或高重叠，能够保证图片完整显示。因为要\"保证图片能够完整显示\"，所以只要操作最长边就能实现。"
-            }
-            SCALE_TYPE_FIT_CENTER -> {
-                str = "按照图片原比例伸缩，直到图片最长边和ImageView对应的宽或高重叠，能够保证图片完整显示。因为要\"保证图片能够完整显示\"，所以只要操作最长边就能实现。"
-            }
-            SCALE_TYPE_FIT_END -> {
-                str = "按照图片原比例伸缩，直到图片最长边和ImageView对应的宽或高重叠，能够保证图片完整显示。因为要\"保证图片能够完整显示\"，所以只要操作最长边就能实现。"
+            SCALE_TYPE_FIT_START, SCALE_TYPE_FIT_CENTER, SCALE_TYPE_FIT_END -> {
+                str = "按照图片原比例伸缩，直到图片的一边和ImageView对应的宽或高重叠，能够保证图片完整显示（基本上图片的一边会和控件对应的一边重叠）。" +
+                        "如果dHeight> dWidth，则使用vWidth/dWidth作为缩放比例，反之使用vHeight/dHeight作为缩放比例。"
             }
             SCALE_TYPE_CENTER -> {
                 str = "不进行任何伸缩，图片中点和ImageView重叠，按照ImageView的宽高裁剪图片，保证图片居中显示，不保证图片能够完整显示或者填满控件。"
