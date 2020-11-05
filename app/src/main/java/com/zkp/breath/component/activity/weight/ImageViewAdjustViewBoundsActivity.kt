7d273b3cc1afe688982c1955c894545f42792666
zwp <1258520250@qq.com>
2020-11-05 16:24:04 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/weight/ImageViewAdjustViewBoundsActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/weight/ImageViewAdjustViewBoundsActivity.kt
@@ -4,11 +4,9 @@ import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import com.blankj.utilcode.util.ImageUtils
 import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.SizeUtils
-import com.blankj.utilcode.util.ToastUtils
 import com.zkp.breath.R
 import com.zkp.breath.component.activity.base.BaseActivity
 import com.zkp.breath.databinding.ActivityIvAdjustViewBoundsBinding
@@ -19,6 +17,8 @@ import com.zkp.breath.databinding.ActivityIvAdjustViewBoundsBinding
  *
  * adjustViewBounds :调整ImageView的边界，使得ImageView和图片有一样的长宽比例。adjustViewBounds只有在
  * ImageView一边固定，一边为wrap_content的时候才有意义。
+ *
+ * 情景：一般在长图且图片比例和ImageView不一致的情况下使用。
  */
 class ImageViewAdjustViewBoundsActivity : BaseActivity() {
     private lateinit var binding: ActivityIvAdjustViewBoundsBinding
@@ -45,18 +45,6 @@ class ImageViewAdjustViewBoundsActivity : BaseActivity() {
             val width = view.width
             val height = view.height
             Log.i("获取测试宽", "width: ${width}, height:$height")
-
-            // 控件宽高比
-            val viewWhRatio = width.div(height * 1f)
-            // 图片宽高比
-            val bmpWhRatio = width1 * 1f / height1
-
-            if (bmpWhRatio > viewWhRatio) {
-                val fl = height * 1f / height1
-                val resultBmpWidth = width1 * fl
-                val resultBmpHeight = height1 * fl
-                Log.i("", ": ");
-            }
         }
     }
 
