e4a860f765a8a61c78b4b7c331b34e3b88ebdb55
zwp <1258520250@qq.com>
2020-11-26 16:08:08 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/AnonymityMethodDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/AnonymityMethodDemo.kt
@@ -1,7 +1,8 @@
 package com.zkp.breath.kotlin
 
 /**
- * 匿名函数：没有名字的函数，其他语法和常规函数一样。其实匿名函数也可以看成是lambda表达式
+ * 匿名函数：没有名字的函数，其他语法和常规函数一样，其实匿名函数就是lambda表达式，所以两者可以相互替代（
+ * 比如一个lambda类型参数可以使用匿名函数传递，也可以使用匿名函数作为函数参数然后使用lambda表达式传入）。
  * 但是注意必须要有val/var修饰的字段指向（都没方法名了,还没有变量名的话怎么调用），否则就需要添加函数名。
  */
 
@@ -10,7 +11,11 @@ var anonymityMethod2 = fun(x: Int, y: Int) = x + y
 val anonymityMethod3 = fun(x: Int, y: Int): Int {
     return x + y
 }
+
+// lambda表达式就是没有函数名，
 val anonymityMethod4: (Int, Int) -> Int = fun(x: Int, y: Int) = x + y
+val anonymityMethod5: (Int, Int) -> Int = { x, y -> x + y }
+
 
 fun lambdaAnonymityMethod(body: (Int, Int) -> Int, x: Int, y: Int) {
     body(x, y)
