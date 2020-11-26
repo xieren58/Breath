231f234a7a7df5177861ccb66477d6c205c72534
zwp <1258520250@qq.com>
2020-11-26 15:40:40 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/jetpack/DataStoreActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/jetpack/DataStoreActivity.kt
@@ -2,11 +2,15 @@ package com.zkp.breath.component.activity.jetpack
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import com.didichuxing.doraemonkit.kit.core.BaseActivity
+import com.zkp.breath.component.activity.base.BaseActivity
 import com.zkp.breath.databinding.ActivityDataStoreBinding
 
 
 class DataStoreActivity : BaseActivity() {
+
+    companion object {
+        private const val PREFERENCE_NAME = "DataStoreDemo"
+    }
 
     private lateinit var binding: ActivityDataStoreBinding
 
@@ -14,6 +18,13 @@ class DataStoreActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDataStoreBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        initView()
+    }
+
+    private fun initView() {
+        // PreferenceDataStoreFactory的createDataStore()
+//        var dataStore: DataStore<Preferences> = this.createDataStore(name = PREFERENCE_NAME)
+
     }
 
 }
