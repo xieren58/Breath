07ab54323c4ee1c50acb90c4de1aab9010eff3dc
zwp <1258520250@qq.com>
2020-12-03 17:11:42 +0800

    update

--- app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
+++ app/src/main/java/com/zkp/breath/kotlin/DataClassDemo.kt
@@ -9,7 +9,7 @@ package com.zkp.breath.kotlin
  *      2.主构函数必须带有全局变量。
  *      3.针对Gson反序列化的情况，必须声明默认无参构造函数或者主沟函数的变量设置默认值，
  *      （防止出现变量为非空类型，但获取的实际值还是null）。https://mp.weixin.qq.com/s/jVRTFTiwTtr7P7vyAj8G7A
- *      4.不能有子类  （好扯蛋的限制）
+ *      4.不能被继承，也不能继承其他类，但能实现接口。（好扯蛋的限制）
  */
 data class DataClass(val s: String, var i: Int) {
     val ss: String = ""
