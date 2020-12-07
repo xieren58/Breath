40b911ce64673511491335807b71f9597be04bfa
zwp <1258520250@qq.com>
2020-12-07 18:00:22 +0800

    update

---
+++ app/src/main/java/com/zkp/breath/component/activity/HandlerActivity.kt
@@ -0,0 +1,17 @@
+package com.zkp.breath.component.activity
+
+import android.os.Bundle
+import com.zkp.breath.R
+import com.zkp.breath.component.activity.base.BaseActivity
+
+/**
+ *
+ */
+class HandlerActivity : BaseActivity(R.layout.activity_handler) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
