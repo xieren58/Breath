83bfb90eec81650f6cf80c79af695cd269cce4aa
zwp <1258520250@qq.com>
2020-12-17 11:25:58 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/jetpack/ViewBindingActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/jetpack/ViewBindingActivity.kt
@@ -2,12 +2,12 @@ package com.zkp.breath.component.activity.jetpack
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
 import com.zkp.breath.component.activity.base.BaseActivity
 import com.zkp.breath.databinding.ActivityViewbindingBinding
 
 /**
  * https://mp.weixin.qq.com/s/BSdzKSOiAWG08epvXN5q2w
+ * https://juejin.cn/post/6905942568467759111
  *
  * ViewBinding优势：
  * 1.防止类型转换异常，修改了控件类型马上进行错误提示。
@@ -20,9 +20,10 @@ class ViewBindingActivity : BaseActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityViewbindingBinding.inflate(LayoutInflater.from(this))
+//         LayoutInflater.from(this)或者getLayoutInflater()
+        binding = ActivityViewbindingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // 调用include内部控件的方式：binding + include布局id + 控件id
+//         调用include内部控件的方式：binding + include布局id + 控件id
         binding.appbar.tvTitle.text = "ViewBindingActivity"
     }
 
