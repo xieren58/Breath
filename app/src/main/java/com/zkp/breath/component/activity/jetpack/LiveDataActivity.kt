f2eea7d5186356d73b6cb14bb2a9e7fe8afa8ee8
zwp <1258520250@qq.com>
2020-12-19 15:25:54 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/jetpack/LiveDataActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/jetpack/LiveDataActivity.kt
@@ -17,18 +17,34 @@ import com.zkp.breath.jetpack.livedata.JetPackLiveDataViewModel
  * https://juejin.cn/post/6844904111112978439
  *
  * LiveData 具有生命周期感知能力的可观察的数据存储器类。（响应生命周期，数据存储器，可观察）
- * 1.响应生命周期：能够感知组件（Fragment、Activity、Service）的生命周期，这里的“组件”皆指实现了LifecycleOwner接口
- *   其实就是获取组件存放LifecycleObserver的map，然后创建LiveData的LifecycleObserver存入。
- * 2.数据存储器：就是存放数据的容器，仅持有 单个且最新 的数据。
- * 3.可观察：可以被观察者订阅，只有在组件出于激活状态（STARTED、RESUMED）才会通知观察者有数据更新。
- * 4.并不是所有数据都需要使用LiveData作为容器，使用LiveData的前提是因为数据需要感知组件的生命周期进行ui显示，请确保
+ *
+ * 1.并不是所有数据都需要使用LiveData作为容器，使用LiveData的前提是因为数据需要感知组件的生命周期进行ui显示，请确保
  *   将用于更新界面的 LiveData 对象存储在 ViewModel 对象中（数据的持久化和感知组件的生命周期）。
+ *
+ * 2.响应生命周期：能够感知组件（Fragment、Activity、Service）的生命周期，这里的“组件”皆指实现了LifecycleOwner接口
+ *   其实就是获取组件存放LifecycleObserver的map，然后创建LiveData的LifecycleObserver存入。在LiveData数据发生
+ *   变化或者绑定的LifecycleOwner生命周期发生变化，都会去调用LiveData的dispatchingValue()方法，内部会检查绑定
+ *   的LifecycleOwner生命周期。
+ *
+ * 3.数据存储器：就是存放数据的容器，仅持有 单个且最新 的数据。
+ *
+ * 4.可观察：可以被观察者订阅，只有在组件出于激活状态（STARTED、RESUMED）才会通知观察者有数据更新。
+ *
  * 5.组件的 onCreate() 是开始观察 LiveData 对象的正确着手点。避免onResume()重复注入同一个观察者（同一个观察者
  *   也会抛出异常）。
+ *
  * 6.LiveData更新值，在主线程必须调用 setValue(T)，工作线程可以使用postValue(T)。
+ *
  * 7.推荐使用observe()注入回调，因为在数据变动情况下可以根据组件的生命周期选择是否触发回调，在组件的onDestroy()方
  *   法还会自动移除注入的回调。observeForever()不会感知生命周期，只要数据发生变动则触发回调，而且需要手动在onDestroy()
  *   方法移除注入的回调。
+ *
+ * 8. androidx fragment 1.2.0 起，添加了新的 Lint 检查，以确保您在从 onCreateView()、onViewCreated() 或
+ *    onActivityCreated() 观察 LiveData 时使用 getViewLifecycleOwner() 而不传this（即Fragment）。因为
+ *    有时候Fragment只是进行视图销毁和重建（onDestroyView()和onCreateView()），由于Fragment没有真正的销毁(
+ *    onDestroy()), 所以LiveData并不会执行移除观察者，这就导致在下次重建视图(onCreateView())又添加了一个LiveData
+ *    观察者，往返多次会添加多个，那么就会造成回调多次（可能发生多次ui刷新），所以应该传入getViewLifecycleOwner()
+ *    感知的是Fragment的视图的生命周期。
  */
 class LiveDataActivity : BaseActivity() {
 
