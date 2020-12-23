f48572a90167beafd9a7988a9f577bc876f9dc93
zwp <1258520250@qq.com>
2020-12-23 15:49:11 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/jetpack/DataBindingActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/jetpack/DataBindingActivity.kt
@@ -12,10 +12,12 @@ import com.zkp.breath.jetpack.databinding.DataBindingViewModel
 
 
 /**
+ *  DataBinding优点：
+ *  1. 具有ViewBinding的优点
+ *  2. 双向数据绑定。
  *
- * DataBinding 会为每个在布局声明 layout 标签的 xml 布局文件生成一个绑定类， 默认情况下类的名称基于布局文件的名称，
- * DataBinding是 MVVM 模式在 Android 上的一种实现。在xml中的变量都是自动空安全的，即便没有在外面赋值，在xml中直接
- * 调用也不会有Null异常（很牛的设计）。
+ *
+ * 在xml中的变量都是自动空安全的，即便没有在外面赋值，在xml中直接调用也不会有Null异常（很牛的设计）。
  *
  * DataBinding各层责任划分：
  * 1. xml：负责定义数据类型和定义变量名，具体控件使用变量名进行数据填充，xml的责任很明确也很简洁，就是在进行数据填充。
@@ -28,11 +30,15 @@ import com.zkp.breath.jetpack.databinding.DataBindingViewModel
  * 自动生成"set+Name（）"的方法，所以在view层进行数据赋值的时候其实就是调用了这个类的"set+Name（）"方法；而BaseObservable
  * 起到双向绑定的作用，内部的操作逻辑可以看成是也在调用"布局文件名 + Binding + Impl"该类的方法。
  *
- * 缺点：1.BaseObservable的实现类作为的Vm层是不具备数据持久化的，不像ViewModel的实现类是具备数据持久化的；
+ * 缺点：
+ *      1.BaseObservable的实现类作为的Vm层是不具备数据持久化的，不像ViewModel的实现类是具备数据持久化的；
  *      2.ViewModel内部的LiveData可以感知组件的生命周期，而且数据可以进行数据刷新回调；而BaseObservable的实现类
  *      本身就具备双向绑定的功能，但是不具备感觉感知组件的生命周期的功能。
  *      3.处理ImageView的加载图片，需要自定义属性后自定义BindingAdapter进行处理。（无法使用import glide进入xml中，
  *        因为需要上下文，而xml没有上下文可以获取）
+ *      4.一定程度上是将操作移入了xml中，导致xml的可读性会下降，职责不单一，代码量上涨。（这就有点拆东墙补西墙的感觉）
+ *      5.当获取到数据的时候，只能选择是否刷新ui，但如果需要根据ui的某些状态（比如是否显示，或者是否已经有内容）进行
+ *        判断是否刷新，那么这个操作要么只能在xml中进行（违反4），可能在xml也无法操作。
  *
  *
  * https://juejin.im/post/6844903609079971854
@@ -57,14 +63,15 @@ class DataBindingActivity : AppCompatActivity() {
          */
         dataBindingViewModel.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
             override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                Log.i("是否改变", "onPropertyChanged: ")
                 // 参数1就是调用注册方法的对象
                 when (propertyId) {
                     BR.num -> {
                         Log.i(tag, "BR.num")
                     }
-                    BR.url -> {
-                        Log.i(tag, "BR.url")
-                    }
+//                    BR.url -> {
+//                        Log.i(tag, "BR.url")
+//                    }
                 }
             }
         })
--- app/src/main/java/com/zkp/breath/jetpack/databinding/DataBindingViewModel.java
+++ app/src/main/java/com/zkp/breath/jetpack/databinding/DataBindingViewModel.java
@@ -8,10 +8,22 @@ import androidx.databinding.Bindable;
 import com.blankj.utilcode.util.ToastUtils;
 import com.zkp.breath.BR;
 
+/**
+ * notifyPropertyChanged()： 只更新对应 BR 的 flag（ui中引用这些变量的会发生刷新），该 BR 的生成通过注解
+ * Bindable 生成，并且回调监视器告知是哪个BR的flag发生了改变。
+ * notifyChange() 刷新所有的值域（ui中引用这些变量的会发生刷新），并且回调监听器。
+ */
 public class DataBindingViewModel extends BaseObservable {
 
+    /**
+     * 如果是 public 修饰符，则可以直接在成员变量上方加上 @Bindable 注解
+     */
+    @Bindable
+    public String name;
+    /**
+     * 如果是 private 修饰符，则在成员变量的 get 方法上添加 @Bindable 注解
+     */
     private String num = "0";
-    private String url = "";
 
     // m层
     private final DataBindingModel mNumDataBindingModel;
@@ -20,27 +32,22 @@ public class DataBindingViewModel extends BaseObservable {
         mNumDataBindingModel = new DataBindingModel();
     }
 
-    /**
-     * @Bindable注解可以提供给xml文件访问
-     */
     @Bindable
     public String getNum() {
         return num;
     }
 
+    /**
+     * 下面的操作缺一不可，刷新数据后通知改变，ui同步刷新
+     */
     public void setNum(String num) {
-        this.num = num;     // 刷新ui数据
-        notifyPropertyChanged(BR.num); // 通知观察者，回调
-    }
-
-    @Bindable
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;     // 刷新ui数据
-        notifyPropertyChanged(BR.url); // 通知观察者，回调
+        this.num = num;
+        this.name = "我是notifyChange";
+        if (Long.parseLong(num) % 2 == 0) {
+            notifyPropertyChanged(BR.num);
+        } else {
+            notifyChange();  //更新所有字段
+        }
     }
 
     public void onClickAdd(View v) {//点击事件处理
