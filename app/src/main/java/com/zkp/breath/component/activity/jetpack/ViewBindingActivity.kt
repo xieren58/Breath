1847bd805ad726239a4f7ffc8cb361bff79eee0d
zwp <1258520250@qq.com>
2020-12-23 11:10:07 +0800

    update

--- app/src/main/java/com/zkp/breath/component/activity/jetpack/ViewBindingActivity.kt
+++ app/src/main/java/com/zkp/breath/component/activity/jetpack/ViewBindingActivity.kt
@@ -9,9 +9,14 @@ import com.zkp.breath.databinding.ActivityViewbindingBinding
  * https://mp.weixin.qq.com/s/BSdzKSOiAWG08epvXN5q2w
  * https://juejin.cn/post/6905942568467759111
  *
+ * 魔法背后：只要开启DataBinding或者ViewBinding会对每个布局xml生成"布局文件名 + Binding"类。
+ * 存放目录：build -> generated -> data_binding_base_class_source_out 目录。
+ *
  * ViewBinding优势：
- * 1.防止类型转换异常，修改了控件类型马上进行错误提示。
- * 2.防止空异常。（一般的获取操作一定是非空）
+ * 1.防止类型安全问题。（binding类的控件成员变量类型是固定的，也会随着xml的变化而变化，不像butterknife不会自动同步xml修改）
+ * 2.防止空异常。（binding类的控件成员变量都是NonNull注解，如果存在不同配置的不同布局文件（如横竖屏）且该控件不
+ *              是存在于所有布局，该处使用 Nullable注解标记）
+ * 3.替换findViewById，减少模板代码（binding类内部其实还是使用findViewById）。
  *
  * kotlin-android-extensions插件(在 2021 年 9 月或之后的 Kotlin 版本中将被移除):
  * 1.使用 Kotlin 合成方法（Synthetic 视图）取代 findViewById，通过引入 kotlinx.android.synthetic 可以直接
@@ -27,10 +32,15 @@ class ViewBindingActivity : BaseActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//         LayoutInflater.from(this)或者getLayoutInflater()
+        /**
+         * LayoutInflater.from(this)或者getLayoutInflater()获取LayoutInflater实例
+         *
+         */
         binding = ActivityViewbindingBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//         调用include内部控件的方式：binding + include布局id + 控件id
+        /**
+         * 调用include内部控件的方式：binding + include布局id + 控件id
+         */
         binding.appbar.tvTitle.text = "ViewBindingActivity"
     }
 
