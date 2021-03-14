64f3ef5d3908b093ba2d38e56b9d81e07aed44a3
zkp <1258520250@qq.com>
2021-03-14 17:24:15 +0800

    1.gradle版本和插件版本升级； 2.升级didi的doraemonkit插件版本，适配gradle版本 3.greendao目前最新插件版本不适配gradle插件版本6，相关使用全部注释，等待官网支持

--- app/src/main/java/com/zkp/breath/database/greendao/entity/Student.java
+++ app/src/main/java/com/zkp/breath/database/greendao/entity/Student.java
@@ -1,130 +1,130 @@
 package com.zkp.breath.database.greendao.entity;
 
-import org.greenrobot.greendao.annotation.Entity;
-import org.greenrobot.greendao.annotation.Id;
-import org.greenrobot.greendao.annotation.Unique;
-import org.greenrobot.greendao.annotation.Generated;
+//import org.greenrobot.greendao.annotation.Entity;
+//import org.greenrobot.greendao.annotation.Id;
+//import org.greenrobot.greendao.annotation.Unique;
+//import org.greenrobot.greendao.annotation.Generated;
 
 /**
  * 一个@Entity的类一定要有一个@Id修饰的id字段，且只能为Long类。
  * 创建完毕后点击as的Build-make model 'app' 会自动生成相应方法，不许认为创建构造方法和get/set方法。
  */
-@Entity
+//@Entity
 public class Student {
-    @Id(autoincrement = true)
-    Long id;
-    @Unique
-    int studentNo;//学号
-    int age; //年龄
-    String telPhone;//手机号
-    String sex; //性别
-    String name;//姓名
-    String address;//家庭住址
-    String schoolName;//学校名字
-    String grade;//几年级
-
-    @Generated(hash = 1071002287)
-    public Student(Long id, int studentNo, int age, String telPhone, String sex,
-                   String name, String address, String schoolName, String grade) {
-        this.id = id;
-        this.studentNo = studentNo;
-        this.age = age;
-        this.telPhone = telPhone;
-        this.sex = sex;
-        this.name = name;
-        this.address = address;
-        this.schoolName = schoolName;
-        this.grade = grade;
-    }
-
-    @Generated(hash = 1556870573)
-    public Student() {
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getStudentNo() {
-        return this.studentNo;
-    }
-
-    public void setStudentNo(int studentNo) {
-        this.studentNo = studentNo;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getTelPhone() {
-        return this.telPhone;
-    }
-
-    public void setTelPhone(String telPhone) {
-        this.telPhone = telPhone;
-    }
-
-    public String getSex() {
-        return this.sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getSchoolName() {
-        return this.schoolName;
-    }
-
-    public void setSchoolName(String schoolName) {
-        this.schoolName = schoolName;
-    }
-
-    public String getGrade() {
-        return this.grade;
-    }
-
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", studentNo=" + studentNo +
-                ", age=" + age +
-                ", telPhone='" + telPhone + '\'' +
-                ", sex='" + sex + '\'' +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", schoolName='" + schoolName + '\'' +
-                ", grade='" + grade + '\'' +
-                '}';
-    }
+//    @Id(autoincrement = true)
+//    Long id;
+//    @Unique
+//    int studentNo;//学号
+//    int age; //年龄
+//    String telPhone;//手机号
+//    String sex; //性别
+//    String name;//姓名
+//    String address;//家庭住址
+//    String schoolName;//学校名字
+//    String grade;//几年级
+//
+//    @Generated(hash = 1071002287)
+//    public Student(Long id, int studentNo, int age, String telPhone, String sex,
+//                   String name, String address, String schoolName, String grade) {
+//        this.id = id;
+//        this.studentNo = studentNo;
+//        this.age = age;
+//        this.telPhone = telPhone;
+//        this.sex = sex;
+//        this.name = name;
+//        this.address = address;
+//        this.schoolName = schoolName;
+//        this.grade = grade;
+//    }
+//
+//    @Generated(hash = 1556870573)
+//    public Student() {
+//    }
+//
+//    public Long getId() {
+//        return this.id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public int getStudentNo() {
+//        return this.studentNo;
+//    }
+//
+//    public void setStudentNo(int studentNo) {
+//        this.studentNo = studentNo;
+//    }
+//
+//    public int getAge() {
+//        return this.age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
+//
+//    public String getTelPhone() {
+//        return this.telPhone;
+//    }
+//
+//    public void setTelPhone(String telPhone) {
+//        this.telPhone = telPhone;
+//    }
+//
+//    public String getSex() {
+//        return this.sex;
+//    }
+//
+//    public void setSex(String sex) {
+//        this.sex = sex;
+//    }
+//
+//    public String getName() {
+//        return this.name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getAddress() {
+//        return this.address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    }
+//
+//    public String getSchoolName() {
+//        return this.schoolName;
+//    }
+//
+//    public void setSchoolName(String schoolName) {
+//        this.schoolName = schoolName;
+//    }
+//
+//    public String getGrade() {
+//        return this.grade;
+//    }
+//
+//    public void setGrade(String grade) {
+//        this.grade = grade;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "id=" + id +
+//                ", studentNo=" + studentNo +
+//                ", age=" + age +
+//                ", telPhone='" + telPhone + '\'' +
+//                ", sex='" + sex + '\'' +
+//                ", name='" + name + '\'' +
+//                ", address='" + address + '\'' +
+//                ", schoolName='" + schoolName + '\'' +
+//                ", grade='" + grade + '\'' +
+//                '}';
+//    }
 }
--- app/src/main/java/com/zkp/breath/database/greendao/manager/GreenDaoManager.java
+++ app/src/main/java/com/zkp/breath/database/greendao/manager/GreenDaoManager.java
@@ -3,117 +3,115 @@ package com.zkp.breath.database.greendao.manager;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.zkp.breath.database.greendao.DaoMaster;
-import com.zkp.breath.database.greendao.DaoSession;
-import com.zkp.breath.database.greendao.StudentDao;
 import com.zkp.breath.database.greendao.entity.Student;
+import com.zkp.breath.jetpack.paging.StudentDao;
 
-import org.greenrobot.greendao.query.DeleteQuery;
-import org.greenrobot.greendao.query.Query;
-import org.greenrobot.greendao.query.QueryBuilder;
+//import org.greenrobot.greendao.query.DeleteQuery;
+//import org.greenrobot.greendao.query.Query;
+//import org.greenrobot.greendao.query.QueryBuilder;
 
 import java.util.List;
 import java.util.Random;
 
 public class GreenDaoManager {
 
-    private DaoSession daoSession;
-    private StudentDao studentDao;
-
-    public void initGreenDao(Context context) {
-//        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, "student.db");
-        CusDevOpenHelper helper = new CusDevOpenHelper(context, "student.db");
-        SQLiteDatabase db = helper.getWritableDatabase();
-        DaoMaster daoMaster = new DaoMaster(db);
-        daoSession = daoMaster.newSession();
-    }
-
-    public void getStudentDao() {
-        if (daoSession == null) {
-            return;
-        }
-        studentDao = daoSession.getStudentDao();
-    }
-
-    /**
-     * android.database.sqlite.SQLiteConstraintException: UNIQUE constraint failed: STUDENT.STUDENT_NO (code 2067 SQLITE_CONSTRAINT_UNIQUE)
-     * studentNo的注解为Unique，插入时在表中已经存在就会报上面的异常
-     */
-    public void insert() {
-        Random mRandom = new Random();
-        for (int i = 0; i < 1000; i++) {
-            Student student = new Student();
-            student.setStudentNo(i);
-            int age = mRandom.nextInt(10) + 10;
-            student.setAge(age);
-            student.setTelPhone("电话" + i);
-            student.setName("姓名" + i);
-            student.setSex(i % 2 == 0 ? "男" : "女");
-            student.setAddress("地址" + i);
-            student.setGrade(age % 10 + "年纪");
-            student.setSchoolName("学校" + i);
-            studentDao.insert(student);
-        }
-    }
-
-    public void insertOrReplace() {
-        Random mRandom = new Random();
-        for (int i = 0; i < 1000; i++) {
-            Student student = new Student();
-            student.setStudentNo(i);
-            int age = mRandom.nextInt(10) + 10;
-            student.setAge(age);
-            student.setTelPhone("电话" + i);
-            student.setName("姓名" + i);
-            student.setSex("不男不女");
-            student.setAddress("地址" + i);
-            student.setGrade(age % 10 + "年纪");
-            student.setSchoolName("学校" + i);
-            studentDao.insertOrReplace(student);
-        }
-    }
-
-    public void deleteData(Student s) {
-        studentDao.delete(s);
-    }
-
-    public void deleteAll() {
-        studentDao.deleteAll();
-    }
-
-    public void updateData(Student s) {
-        studentDao.update(s);
-    }
-
-    public List<Student> queryAll() {
-        return studentDao.loadAll();
-    }
-
-    public List<Student> queryData(String id) {
-        return studentDao.queryRaw("where _id = ?", id);
-    }
-
-    public List<Student> queryList() {
-        QueryBuilder<Student> studentQueryBuilder = studentDao.queryBuilder();
-        return studentQueryBuilder.list();
-    }
-
-    /**
-     * 如果
-     */
-    public List<Student> queryWhere() {
-        QueryBuilder<Student> qb = studentDao.queryBuilder();
-        Query<Student> query = qb.where(StudentDao.Properties.Sex.eq("男")).orderAsc(StudentDao.Properties.Id).build();
-        return query.list();
-    }
-
-    public boolean deleteItem() {
-        QueryBuilder<Student> where = studentDao.queryBuilder().where(StudentDao.Properties.Id.gt(5));
-        // 构建删除器
-        DeleteQuery<Student> deleteQuery = where.buildDelete();
-        // 执行删除
-        deleteQuery.executeDeleteWithoutDetachingEntities();
-        return false;
-    }
+//    private DaoSession daoSession;
+//    private StudentDao studentDao;
+//
+//    public void initGreenDao(Context context) {
+////        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, "student.db");
+//        CusDevOpenHelper helper = new CusDevOpenHelper(context, "student.db");
+//        SQLiteDatabase db = helper.getWritableDatabase();
+//        DaoMaster daoMaster = new DaoMaster(db);
+//        daoSession = daoMaster.newSession();
+//    }
+//
+//    public void getStudentDao() {
+//        if (daoSession == null) {
+//            return;
+//        }
+//        studentDao = daoSession.getStudentDao();
+//    }
+//
+//    /**
+//     * android.database.sqlite.SQLiteConstraintException: UNIQUE constraint failed: STUDENT.STUDENT_NO (code 2067 SQLITE_CONSTRAINT_UNIQUE)
+//     * studentNo的注解为Unique，插入时在表中已经存在就会报上面的异常
+//     */
+//    public void insert() {
+//        Random mRandom = new Random();
+//        for (int i = 0; i < 1000; i++) {
+//            Student student = new Student();
+//            student.setStudentNo(i);
+//            int age = mRandom.nextInt(10) + 10;
+//            student.setAge(age);
+//            student.setTelPhone("电话" + i);
+//            student.setName("姓名" + i);
+//            student.setSex(i % 2 == 0 ? "男" : "女");
+//            student.setAddress("地址" + i);
+//            student.setGrade(age % 10 + "年纪");
+//            student.setSchoolName("学校" + i);
+//            studentDao.insert(student);
+//        }
+//    }
+//
+//    public void insertOrReplace() {
+//        Random mRandom = new Random();
+//        for (int i = 0; i < 1000; i++) {
+//            Student student = new Student();
+//            student.setStudentNo(i);
+//            int age = mRandom.nextInt(10) + 10;
+//            student.setAge(age);
+//            student.setTelPhone("电话" + i);
+//            student.setName("姓名" + i);
+//            student.setSex("不男不女");
+//            student.setAddress("地址" + i);
+//            student.setGrade(age % 10 + "年纪");
+//            student.setSchoolName("学校" + i);
+//            studentDao.insertOrReplace(student);
+//        }
+//    }
+//
+//    public void deleteData(Student s) {
+//        studentDao.delete(s);
+//    }
+//
+//    public void deleteAll() {
+//        studentDao.deleteAll();
+//    }
+//
+//    public void updateData(Student s) {
+//        studentDao.update(s);
+//    }
+//
+//    public List<Student> queryAll() {
+//        return studentDao.loadAll();
+//    }
+//
+//    public List<Student> queryData(String id) {
+//        return studentDao.queryRaw("where _id = ?", id);
+//    }
+//
+//    public List<Student> queryList() {
+//        QueryBuilder<Student> studentQueryBuilder = studentDao.queryBuilder();
+//        return studentQueryBuilder.list();
+//    }
+//
+//    /**
+//     * 如果
+//     */
+//    public List<Student> queryWhere() {
+//        QueryBuilder<Student> qb = studentDao.queryBuilder();
+//        Query<Student> query = qb.where(StudentDao.Properties.Sex.eq("男")).orderAsc(StudentDao.Properties.Id).build();
+//        return query.list();
+//    }
+//
+//    public boolean deleteItem() {
+//        QueryBuilder<Student> where = studentDao.queryBuilder().where(StudentDao.Properties.Id.gt(5));
+//        // 构建删除器
+//        DeleteQuery<Student> deleteQuery = where.buildDelete();
+//        // 执行删除
+//        deleteQuery.executeDeleteWithoutDetachingEntities();
+//        return false;
+//    }
 
 }
