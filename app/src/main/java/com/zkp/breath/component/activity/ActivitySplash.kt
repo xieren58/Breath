25082e3e337b3e730e624799d3b5d7d8af00e92b
1258520250@qq.com <zkp@2116&>
2021-10-16 16:49:17 +0800

    检查权限不在闪屏页进行，防止出现android.view.WindowLeaked

--- app/src/main/java/com/zkp/breath/component/activity/ActivitySplash.kt
+++ app/src/main/java/com/zkp/breath/component/activity/ActivitySplash.kt
@@ -8,6 +8,7 @@ import com.blankj.utilcode.constant.PermissionConstants
 import com.blankj.utilcode.util.ActivityUtils
 import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.PermissionUtils
+import com.blankj.utilcode.util.ThreadUtils
 import com.zkp.breath.MainActivity
 import com.zkp.breath.R
 import com.zkp.breath.component.activity.base.BaseActivity
@@ -16,40 +17,8 @@ class ActivitySplash : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestPermission()
+        ActivityUtils.startActivity(MainActivity::class.java)
+        ActivityUtils.finishActivity(this@ActivitySplash)
     }
 
-    private fun requestPermission() {
-        PermissionUtils.permission(PermissionConstants.STORAGE, PermissionConstants.MICROPHONE)
-                .rationale { activity, shouldRequest ->
-                    Log.i(ACTIVITY_TAG, "rationale")
-                    shouldRequest?.again(true)
-                }
-                .callback(object : PermissionUtils.FullCallback {
-                    override fun onGranted(granted: MutableList<String>) {
-                        Log.i(ACTIVITY_TAG, "onGranted")
-                        ActivityUtils.startActivity(MainActivity::class.java)
-                        ActivityUtils.finishActivity(this@ActivitySplash)
-                    }
-
-                    override fun onDenied(deniedForever: MutableList<String>, denied: MutableList<String>) {
-                        Log.i(ACTIVITY_TAG, "onDenied")
-                        if (deniedForever.isNotEmpty()) {
-                            // 防止title和message无显示
-                            val builder: AlertDialog.Builder = AlertDialog.Builder(
-                                    this@ActivitySplash,
-                                    R.style.Theme_AppCompat_Light_Dialog_Alert)
-                            builder.setCancelable(false)
-                            val alertDialog = builder.setTitle("提示！")
-                                    .setMessage("请前往设置->应用" + "->权限中打开读写相关权限，否则功能无法正常运行！")
-                                    .setPositiveButton("确定") { dialog: DialogInterface?, which: Int ->
-                                        this@ActivitySplash.finish()
-                                        AppUtils.launchAppDetailsSettings()
-                                    }.show()
-                        } else {
-                            ActivityUtils.finishActivity(ActivitySplash::class.java)
-                        }
-                    }
-                }).request()
-    }
 }
