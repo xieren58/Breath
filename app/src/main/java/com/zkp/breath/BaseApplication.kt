fa8bd22eae7119d87aadf1fb9959c49485c516cc
1258520250@qq.com <zkp@2116&>
2021-10-22 16:38:53 +0800

    添加重建逻辑

--- app/src/main/java/com/zkp/breath/BaseApplication.kt
+++ app/src/main/java/com/zkp/breath/BaseApplication.kt
@@ -1,15 +1,15 @@
 package com.zkp.breath
 
 import android.app.Activity
+import android.os.Build
+import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.multidex.MultiDexApplication
 import com.alibaba.android.arouter.launcher.ARouter
-import com.blankj.utilcode.util.AppUtils
-import com.blankj.utilcode.util.ProcessUtils
-import com.blankj.utilcode.util.Utils
+import com.blankj.utilcode.util.*
 import com.didichuxing.doraemonkit.DoraemonKit
 import com.simple.spiderman.SpiderMan
 import com.zkp.breath.mmkv.template.AppConfiguration
@@ -33,7 +33,7 @@ class BaseApplication : MultiDexApplication() {
             initAssistKit()
             // 初始化mmkv
             AppConfiguration.getDefault(this)
-            initUmAnalytics()
+//            initUmAnalytics()
             appStatusChanged()
         }
 //        initUmPushOnUmPushProcess()
@@ -47,6 +47,58 @@ class BaseApplication : MultiDexApplication() {
         ProcessLifecycleOwner.get().lifecycle.addObserver(observer)
         // 旧的监听应用前后台切换的方式
         AppUtils.registerAppStatusChangedListener(onAppStatusChangedListener)
+        // 监听所有activity的生命周期变化
+        registerActivityLifecycleCallbacks(mActivityLifecycleCallbacks)
+    }
+
+    var isKilled = true
+
+    /**
+     * Activity 生命周期监听
+     */
+    private val mActivityLifecycleCallbacks = object : ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+            /**
+             * 重建发生进行重启app的操作，防止其余页面或者当前页面的资源被回收导致出现异常。
+             * debug环境下不使用，让问题暴露才能更好解决。
+             */
+            if (!BuildConfig.DEBUG) {
+                if (StringUtils.equals(ActivityUtils.getLauncherActivity(), activity::class.java.name)) {
+                    // 启动activity则设置为false
+                    isKilled = false
+                } else {
+                    // 非启动页且isKilled为true则表示application被重建，重启Launcher页，并且杀掉顶部重建的activity
+                    // 保证所有初始化流程不受影响
+                    if (isKilled) {
+                        val topActivity = ActivityUtils.getTopActivity()
+                        Log.i("onActivityCreated", "发生app重建，发生页面: ${topActivity::class.java.name}")
+                        ActivityUtils.startLauncherActivity()
+                        ActivityUtils.finishActivity(topActivity)
+                    }
+                }
+            }
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+        }
     }
 
     val observer = object : DefaultLifecycleObserver {
