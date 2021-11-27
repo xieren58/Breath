8334c9310707518055745ea9f9c2245a59800149
1258520250@qq.com <zkp@2116&>
2021-11-27 17:29:51 +0800

    android12适配

---
+++ app/src/main/java/com/zkp/breath/component/activity/sdkVersion/Android12Activity.kt
@@ -0,0 +1,9 @@
+package com.zkp.breath.component.activity.sdkVersion
+
+/**
+ * https://mp.weixin.qq.com/s/clp9_MVWXm5ZtSPxXJZF2A
+ * https://juejin.cn/post/6979059991634575391
+ */
+class Android12Activity  {
+
+}
